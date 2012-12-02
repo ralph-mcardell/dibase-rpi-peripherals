8c286577051516bc71aea3ff1050c5cfcb40f505
Ralph McArdell <ralph@dibasepi1>
2012-12-02 17:53:12 +0000

    Reworked mapped_pin_id as template class, added rpi_version_mapped_pin_id template struct and connector pin number mapping types p1_pin and p5_pin with board version support.

--- src/pin_id.h
+++ src/pin_id.h
@@ -9,6 +9,7 @@
  #define DIBASE_RPI_PERIPHERALS_PIN_ID_H
 
  #include "peridef.h"
+ #include "rpi_info.h"
  #include <stdexcept>
 
 namespace dibase { namespace rpi {
@@ -47,7 +48,9 @@ namespace dibase { namespace rpi {
       {
         if ( v > max_id ) // v < min_id not needed with v unsigned and min_id=0
           {
-            throw std::invalid_argument("pin_id::pin_id: Invalid GPIO pin number");
+            throw std::invalid_argument( "pin_id::pin_id: Invalid pin number or"
+                                         " board revision is not supported"
+                                       );
           }
       }
 
@@ -62,56 +65,159 @@ namespace dibase { namespace rpi {
       pin_id_int_t  value;  ///< Wrapped integer value of the pin id.
     };
     
-  /// @brief Return a pin_id with a value produced via a mapping array
-  ///
-  /// The returned pin_id's integer value is that given by the index key into
-  /// a map array of integers viz:
-  ///
-  /// returned pin_id = pin_id( map[key] )
-  ///
-  /// @param[in]  key     Mapping key value
-  /// @param[in]  map     Mapping array of pin_id integer values. If an array
-  ///                     slot does not map to a valid pin_id use an invalid
-  ///                     pin_id value to force a pin_id creation failure.
-  /// @param[in] map_size Number of pin_id integer values in map.
-  /// @return a pin_id created using the mapped value for the key:
-  ///         pin_id(map[key])
-  /// @exception  std::invalid_argument raised if key >= map_size or map[key]
-  ///             is not a valid pin_id value.
-    inline
-    pin_id mapped_pin_id
-    ( pin_id_int_t key
-    , pin_id_int_t const * map
-    , std::size_t map_size
-    )
+  /// @brief Subclass of pin_id that initialises value using a key into a map
+  /// mapped_pin_id is an intermediate type not intended for use in client code.
+  /// @param MAP_KEY_SIZE   Number of pin keys in a mapping vector as these
+  ///                       will be constant for each connector's pin mapping.
+    template <std::size_t MAP_KEY_SIZE>
+    struct mapped_pin_id : public pin_id
     {
-      if ( key >= map_size )
-        {
-          throw std::invalid_argument("mapped_pin_id: Invalid GPIO pin number");
-        }
-      return pin_id(map[key]);
-    }
+    /// @brief Create mapped_pin_id from key and mapping array
+    ///
+    /// The base pin_id is intialised by the value of a map array item at a key
+    /// index viz: pin_id( map[key] )
+    ///
+    /// @param[in]  key     Mapping key value
+    /// @param[in]  map     Mapping array of pin_id integer values. If an array
+    ///                     slot does not map to a valid pin_id use an invalid
+    ///                     pin_id value to force a pin_id creation failure.
+    /// @return a pin_id created using the mapped value for the key:
+    ///         pin_id(map[key])
+    /// @exception  std::invalid_argument raised if key >= map_size or map[key]
+    ///             is not a valid pin_id value.
+      mapped_pin_id
+      ( pin_id_int_t key
+      , pin_id_int_t const * map
+      )
+      : pin_id(key>=MAP_KEY_SIZE ? -1 : map[key])
+      {}
+    };
+
+  /// @brief Subclass of mapped_pin_id that notes Raspberry Pi board version
+  /// rpi_version_mapped_pin_id is an intermediate type template not intended
+  /// for use in client code.
+  /// @param MAP_KEY_SIZE     Number of pin keys in a mapping vector.
+  ///                         Needed for 2D [version][pin key] array type
+  ///                         decls. Constant for each connector's pin mapping.
+  /// @param MAP_VERSION_SIZE Number of board versions' mapping vectors.
+  ///                         Constant for all connectors' pin mappings.
+    template <std::size_t MAP_KEY_SIZE, std::size_t MAP_VERSION_SIZE>
+    struct rpi_version_mapped_pin_id : public mapped_pin_id<MAP_KEY_SIZE>
+    {
+      typedef mapped_pin_id<MAP_KEY_SIZE> super;  ///< Super class type alias
+
+    /// @brief Create rpi_version_mapped_pin_id from key and mapping array
+    ///
+    /// The base pin_id is intialised by the value in a map vector in a 2D array
+    /// selected based on the Raspberry Pi board version item at a key
+    /// index viz: pin_id( map[rpi_board_version_index][key] )
+    /// The Raspberry Pi board version index value is obtained from the 
+    /// rpi_info.index_version() member function.
+    ///
+    /// @param[in]  key             Mapping key value
+    /// @param[in]  map             Mapping array of pin_id integer values, one
+    ///                             mapping vector for each supported Raspberry
+    ///                             Pi board version. If an array  slot does
+    ///                             not map to a valid pin_id use an invalid
+    ///                             pin_id value to force a failure.
+    /// @return a pin_id created using the mapped value for the key:
+    ///         pin_id(map[key])
+    /// @exception  std::invalid_argument raised if reported board version >=
+    ///             MAP_VERSION_SIZE, key >= MAP_KEY_SIZE or map[key] is not a
+    ///             valid pin_id value.
+      rpi_version_mapped_pin_id
+      ( pin_id_int_t key
+      , pin_id_int_t const map[][MAP_KEY_SIZE]
+      )
+      : super( rpi_info().index_version()>=MAP_VERSION_SIZE ? -1 : key
+             , map[rpi_info().index_version()]
+             )
+      {}
+    };
+
+  /// @brief Number of versions of Raspberry Pi connectors pin out supported
+    std::size_t const pinout_versions{2};
 
   /// @brief Number of pins on Raspberry Pi P1 connector
     std::size_t const p1_pin_count{26};
 
-  /// @brief Number of versions of Raspberry Pi P1 connector pin connections
-    std::size_t const p1_versions{2};
-
-  /// @brief Number of slotes for Raspberry Pi P1 connector pin_id map.
+  /// @brief Number of slots for Raspberry Pi P1 connector pin_id map.
   ///
   /// As connector pins count from 1 and C/C++ arrays indexes count from 0 the
   /// map includes an extra slot for the non-existent pin 0.
     std::size_t const p1_map_size{p1_pin_count+1};
-
-    /// @brief Mapping arrays for each version of Raspberry Pi P1 connector pins
+    
+  /// @brief Mapping arrays for each version of Raspberry Pi P1 connector pins
   ///
   /// Each Raspberry Pi P1 connector version has its own vector mapping P1
   /// pin numbers to BCM2835 GPIO pin numbers (that is, pin_id values). One
   /// extra slot is required for the non-existent pin 0, which resolves to an
   /// invalid GPIO pin id value.
-    extern pin_id_int_t p1_gpio_pin_map[p1_versions][p1_map_size];
+    extern pin_id_int_t p1_gpio_pin_map[pinout_versions][p1_map_size];
 
+  /// @brief Raspberry Pi P1 connector pin representation
+  /// 
+  /// A rpi_version_mapped_pin_id specialisation sub-type that takes simply
+  /// a value representing a P1 connector pin number. It converts to an
+  /// integer having the value of the underlying BCM2835 GPIO pin id value or,
+  /// for invalid, non-GPIO pin numbers or unsupported board versions throws
+  /// and exception.
+    struct p1_pin : rpi_version_mapped_pin_id<p1_map_size, pinout_versions>
+    {
+    /// @brief Super class type alias
+      typedef rpi_version_mapped_pin_id<p1_map_size, pinout_versions> super;
+
+    /// @brief Construct from a P1 connector pin_number
+    /// @param[in] pin_number   P1 GPIO pin number (1..26)
+    /// @exception std::invalid_argument raised if pin_number out of range or
+    ///             represents a pin with a non-GPIO function or the Raspberry Pi
+    ///             board has an unsupported version number.
+      p1_pin( pin_id_int_t pin_number )
+      : super( pin_number, p1_gpio_pin_map )
+      {}
+    };
+
+  /// @brief Number of pins on Raspberry Pi P5 connector (V2 baords onwards)
+    std::size_t const p5_pin_count{8};
+
+  /// @brief Number of slotes for Raspberry Pi P5 connector pin_id map.
+  ///
+  /// As connector pins count from 1 and C/C++ arrays indexes count from 0 the
+  /// map includes an extra slot for the non-existent pin 0.
+    std::size_t const p5_map_size{p5_pin_count+1};
+
+  /// @brief Mapping arrays for each version of Raspberry Pi P5 connector pins
+  ///
+  /// Each Raspberry Pi P5 connector version has its own vector mapping P5
+  /// pin numbers to BCM2835 GPIO pin numbers (that is, pin_id values). One
+  /// extra slot is required for the non-existent pin 0, which resolves to an
+  /// invalid GPIO pin id value. All mappings for V1 P5 map to invalid pins as 
+  /// there is no P5 on V1 boards.
+    extern pin_id_int_t p5_gpio_pin_map[pinout_versions][p5_map_size];
+
+  /// @brief Raspberry Pi P5 connector pin representation
+  /// 
+  /// A rpi_version_mapped_pin_id specialisation sub-type that takes simply
+  /// a value representing a P5 connector pin number. It converts to an
+  /// integer having the value of the underlying BCM2835 GPIO pin id value or,
+  /// for invalid, non-GPIO pin numbers or unsupported board versions throws
+  /// and exception.
+    struct p5_pin : rpi_version_mapped_pin_id<p5_map_size, pinout_versions>
+    {
+    /// @brief Super class type alias
+      typedef rpi_version_mapped_pin_id<p5_map_size, pinout_versions> super;
+
+    /// @brief Construct from a P5 connector pin_number
+    /// @param[in] pin_number   P5 GPIO pin number (1..8, 3..6 valid GPIO pins)
+    /// @exception std::invalid_argument raised if pin_number out of range or
+    ///             represents a pin with a non-GPIO function or the Raspberry
+    ///             Pi board has an unsupported version number or does not
+    ///             support P5.
+      p5_pin( pin_id_int_t pin_number )
+      : super( pin_number, p5_gpio_pin_map )
+      {}
+    };
   } // namespace peripherals closed
 }} // namespaces rpi and dibase closed
 #endif // DIBASE_RPI_PERIPHERALS_PIN_ID_H
+
--- src/tests/pin_id_unittests.cpp
+++ src/tests/pin_id_unittests.cpp
@@ -58,10 +58,11 @@ TEST_CASE( "Unit_tests/pin_id/mapped_pin_id_returns_pin_id_via_mapping"
          )
 {
   pin_id_int_t map[] = {20,21,22,23,24,24};
-  std::size_t map_size{sizeof(map)/sizeof(pin_id_int_t)};
+  std::size_t const map_size{sizeof(map)/sizeof(pin_id_int_t)};
+
   for ( std::size_t id=0; id!=map_size; ++id )
     {
-      CHECK( mapped_pin_id(id, map, map_size)==map[id] );
+      CHECK( mapped_pin_id<map_size>(id, map)==map[id] );
     }
 }
 
@@ -70,9 +71,10 @@ TEST_CASE( "Unit_tests/pin_id/bad_mapped_pin_id_key_throws"
          )
 {
   pin_id_int_t map[] = {20,21,22,23,24,24};
-  std::size_t map_size{sizeof(map)/sizeof(pin_id_int_t)};
-  REQUIRE_THROWS_AS(mapped_pin_id(map_size, map, map_size), std::invalid_argument);
-  REQUIRE_THROWS_AS(mapped_pin_id(-1, map, map_size), std::invalid_argument);
+  std::size_t const map_size{sizeof(map)/sizeof(pin_id_int_t)};
+  typedef mapped_pin_id<map_size>  bad_mapped_pin_id_t;
+  REQUIRE_THROWS_AS(bad_mapped_pin_id_t(map_size, map), std::invalid_argument);
+  REQUIRE_THROWS_AS(bad_mapped_pin_id_t(-1, map), std::invalid_argument);
 }
 
 TEST_CASE( "Unit_tests/pin_id/bad_mapped_pin_id_value_throws"
@@ -80,9 +82,10 @@ TEST_CASE( "Unit_tests/pin_id/bad_mapped_pin_id_value_throws"
          )
 {
   pin_id_int_t map[] = {max_gpio_number+1,min_gpio_number-1};
-  std::size_t map_size{sizeof(map)/sizeof(pin_id_int_t)};
-  REQUIRE_THROWS_AS(mapped_pin_id(0, map, map_size), std::invalid_argument);
-  REQUIRE_THROWS_AS(mapped_pin_id(1, map, map_size), std::invalid_argument);
+  std::size_t const map_size{sizeof(map)/sizeof(pin_id_int_t)};
+  typedef mapped_pin_id<map_size>  bad_mapped_pin_id_t;
+  REQUIRE_THROWS_AS(bad_mapped_pin_id_t(0, map), std::invalid_argument);
+  REQUIRE_THROWS_AS(bad_mapped_pin_id_t(1, map), std::invalid_argument);
 }
 
 // from Raspberry Pi V1 schematic...
@@ -92,6 +95,8 @@ pin_id_int_t p1_v1_gpio_chip_ids[]
                         = { 0, 1, 4,14,15,17,18,21,22,23,24,10, 9,25,11, 8, 7 };
 std::size_t const number_of_p1_gpio_pins{17};
 
+typedef mapped_pin_id<p1_map_size> p1_mapped_pin_id;
+
 TEST_CASE( "Unit_tests/pin_id/P1_V1_pins_map_as_expected"
          , "P1 version 1 connector pins map to expected pin_id values"
          )
@@ -99,7 +104,8 @@ TEST_CASE( "Unit_tests/pin_id/P1_V1_pins_map_as_expected"
   REQUIRE((sizeof(p1_gpio_pins)/sizeof(pin_id_int_t))==number_of_p1_gpio_pins);
   for (std::size_t pin_idx=0; pin_idx!=number_of_p1_gpio_pins;++pin_idx)
     {
-      CHECK(mapped_pin_id(p1_gpio_pins[pin_idx], p1_gpio_pin_map[0], p1_map_size)==p1_v1_gpio_chip_ids[pin_idx]);
+      CHECK( p1_mapped_pin_id(p1_gpio_pins[pin_idx], p1_gpio_pin_map[0])
+                                            ==p1_v1_gpio_chip_ids[pin_idx] );
     }
 }
 
@@ -114,11 +120,13 @@ TEST_CASE( "Unit_tests/pin_id/P1_V2_pins_map_as_expected"
   REQUIRE((sizeof(p1_gpio_pins)/sizeof(pin_id_int_t))==number_of_p1_gpio_pins);
   for (std::size_t pin_idx=0; pin_idx!=number_of_p1_gpio_pins;++pin_idx)
     {
-      CHECK(mapped_pin_id(p1_gpio_pins[pin_idx], p1_gpio_pin_map[1], p1_map_size)==p1_v2_gpio_chip_ids[pin_idx]);
+      CHECK( p1_mapped_pin_id(p1_gpio_pins[pin_idx], p1_gpio_pin_map[1])
+                                            ==p1_v2_gpio_chip_ids[pin_idx] );
     }
 }
 
 // from Raspberry Pi V1 schematic...
+// Note: includes extra non-GPIO pin slot for phantom pin 0
 pin_id_int_t p1_non_gpio_pins[] = { 0, 1, 2, 4, 6, 9, 14, 17, 20, 25 };
 std::size_t const number_of_p1_non_gpio_pins{10};
 TEST_CASE( "Unit_tests/pin_id/P1_V1_V2_non_gpio_pins_throw"
@@ -128,7 +136,240 @@ TEST_CASE( "Unit_tests/pin_id/P1_V1_V2_non_gpio_pins_throw"
   REQUIRE((sizeof(p1_non_gpio_pins)/sizeof(pin_id_int_t))==number_of_p1_non_gpio_pins);
   for (std::size_t pin_idx=0; pin_idx!=number_of_p1_non_gpio_pins;++pin_idx)
     {
-      REQUIRE_THROWS_AS(mapped_pin_id(p1_non_gpio_pins[pin_idx], p1_gpio_pin_map[0], p1_map_size), std::invalid_argument);
-      REQUIRE_THROWS_AS(mapped_pin_id(p1_non_gpio_pins[pin_idx], p1_gpio_pin_map[1], p1_map_size), std::invalid_argument);
+      REQUIRE_THROWS_AS(p1_mapped_pin_id(p1_non_gpio_pins[pin_idx], p1_gpio_pin_map[0]), std::invalid_argument);
+      REQUIRE_THROWS_AS(p1_mapped_pin_id(p1_non_gpio_pins[pin_idx], p1_gpio_pin_map[1]), std::invalid_argument);
     }
+}
+
+TEST_CASE( "Unit_tests/pin_id/P1_V1_V2_map_size_pin_count"
+         , "Sum of GPIO and non-GPIO P1 pins should equal map size, pin count+1"
+         )
+{
+  CHECK((number_of_p1_gpio_pins+number_of_p1_non_gpio_pins)==p1_map_size);
+  CHECK((number_of_p1_gpio_pins+number_of_p1_non_gpio_pins-1)==p1_pin_count);
+}
+
+// from Raspberry Pi V2 schematic...
+pin_id_int_t p5_gpio_pins[]         = {  3,  4,  5,  6 };
+pin_id_int_t p5_v2_gpio_chip_ids[]  = { 28, 29, 30, 31 };
+std::size_t const number_of_p5_gpio_pins{4};
+
+typedef mapped_pin_id<p5_map_size> p5_mapped_pin_id;
+
+TEST_CASE( "Unit_tests/pin_id/P5_V2_pins_map_as_expected"
+         , "P5 version 2 connector pins map to expected pin_id values"
+         )
+{
+  REQUIRE((sizeof(p5_gpio_pins)/sizeof(pin_id_int_t))==number_of_p5_gpio_pins);
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p5_gpio_pins;++pin_idx)
+    {
+      CHECK( p5_mapped_pin_id(p5_gpio_pins[pin_idx], p5_gpio_pin_map[1])
+                                            ==p5_v2_gpio_chip_ids[pin_idx] );
+    }
+}
+
+// from Raspberry Pi V2 schematic...
+// Note: includes extra non-GPIO pin slot for phantom pin 0
+pin_id_int_t p5_v2_non_gpio_pins[] = { 0, 1, 2, 7, 8 };
+std::size_t const number_of_p5_v2_non_gpio_pins{5};
+TEST_CASE( "Unit_tests/pin_id/P5_V2_non_gpio_pins_throw"
+         , "Non-GPIO P5 version 2 connector pins throw std::invalid_argument"
+         )
+{
+  REQUIRE((sizeof(p5_v2_non_gpio_pins)/sizeof(pin_id_int_t))==number_of_p5_v2_non_gpio_pins);
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p5_v2_non_gpio_pins;++pin_idx)
+    {
+      REQUIRE_THROWS_AS(p5_mapped_pin_id(p5_v2_non_gpio_pins[pin_idx], p5_gpio_pin_map[1]), std::invalid_argument);
+    }
+}
+
+// Version 1 boards have no P5 support so all pins effectively non-GPIO
+// Note: includes extra non-GPIO pin slot for phantom pin 0
+std::size_t const number_of_p5_v1_non_gpio_pins{9};
+TEST_CASE( "Unit_tests/pin_id/P5_V1_non_gpio_pins_throw"
+         , "All v.1 non-existent P5 connector pins throw std::invalid_argument"
+         )
+{
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p5_v1_non_gpio_pins;++pin_idx)
+    {
+      REQUIRE_THROWS_AS(p5_mapped_pin_id(pin_idx, p5_gpio_pin_map[0]), std::invalid_argument);
+    }
+}
+
+TEST_CASE( "Unit_tests/pin_id/P5_V1_V2_map_size_pin_count"
+         , "Sum of GPIO and non-GPIO P5 pins should equal map size, pin count+1"
+         )
+{
+  CHECK((number_of_p5_gpio_pins+number_of_p5_v2_non_gpio_pins)==p5_map_size);
+  CHECK((number_of_p5_gpio_pins+number_of_p5_v2_non_gpio_pins-1)==p5_pin_count);
+  CHECK(number_of_p5_v1_non_gpio_pins==p5_map_size);
+}
+
+// Additional test scafolding to allow mocking Raspberry Pi version information
+#include "rpi_info.h"
+#include "rpi_init.h"
+
+using namespace dibase::rpi;
+
+namespace dibase { namespace rpi {
+namespace internal
+{
+  extern rpi_init * rpi_initialiser;
+}
+}}
+
+struct test_pin_id_rpi_init : internal::rpi_init
+{
+  test_pin_id_rpi_init()
+  : test_rpi_board_version(-1)
+  {
+  }
+
+  virtual std::size_t init_major_version(); // override
+
+  std::size_t test_rpi_board_version;
+};
+
+std::size_t test_pin_id_rpi_init::init_major_version()
+{
+  return test_rpi_board_version;
+}
+
+static test_pin_id_rpi_init test_rpi_initialiser;
+
+struct test_rpi_version_init
+{
+  test_rpi_version_init()
+  : original_pointer(internal::rpi_initialiser)
+  {
+    internal::rpi_initialiser = &test_rpi_initialiser;
+  }
+  ~test_rpi_version_init()
+  {
+    internal::rpi_initialiser = original_pointer;
+  }
+private:
+  internal::rpi_init * const original_pointer;
+};
+
+typedef rpi_version_mapped_pin_id<p1_map_size,pinout_versions>
+                                                      tst_version_mapped_pin_id;
+TEST_CASE( "Unit_tests/pin_id/rpi_version_mapped_pin_id_as_expected"
+         , "rpi_version_mapped_pin_id has expected values for valid versions"
+         )
+{
+  test_rpi_version_init setup;
+  test_rpi_initialiser.test_rpi_board_version=1;
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p1_gpio_pins;++pin_idx)
+    {
+      CHECK( tst_version_mapped_pin_id( p1_gpio_pins[pin_idx]
+                                      , p1_gpio_pin_map
+                                      )==p1_v1_gpio_chip_ids[pin_idx]
+            );
+    }
+  test_rpi_initialiser.test_rpi_board_version=2; 
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p1_gpio_pins;++pin_idx)
+    {
+      CHECK( tst_version_mapped_pin_id( p1_gpio_pins[pin_idx]
+                                      , p1_gpio_pin_map
+                                      )==p1_v2_gpio_chip_ids[pin_idx]
+            );
+    }
+}
+
+TEST_CASE( "Unit_tests/pin_id/rpi_version_mapped_pin_id_fail_as_expected"
+         , "rpi_version_mapped_pin_id fail for invalid pin id and/or version"
+         )
+{
+  test_rpi_version_init setup;
+  test_rpi_initialiser.test_rpi_board_version=1;
+// Non-GPIO connector pin number in map
+  REQUIRE_THROWS_AS( tst_version_mapped_pin_id( p1_non_gpio_pins[0]
+                                              , p1_gpio_pin_map
+                                              )
+                    , std::invalid_argument
+                    );
+// Bad connector pin number
+  REQUIRE_THROWS_AS( tst_version_mapped_pin_id( p1_map_size
+                                              , p1_gpio_pin_map
+                                              )
+                    , std::invalid_argument
+                    );
+// Bad rpi board version
+  test_rpi_initialiser.test_rpi_board_version=pinout_versions+1;
+  REQUIRE_THROWS_AS( tst_version_mapped_pin_id( p1_non_gpio_pins[0]
+                                              , p1_gpio_pin_map
+                                              )
+                    , std::invalid_argument
+                    );
+}
+
+TEST_CASE( "Unit_tests/pin_id/good_p1_pin_arguments_produce_expected_pin-ids"
+         , "p1_pin GPIO pin numbers should produce correct mapped pin_id value"
+         )
+{
+  test_rpi_version_init setup;
+  test_rpi_initialiser.test_rpi_board_version=1;
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p1_gpio_pins;++pin_idx)
+    {
+      CHECK( p1_pin(p1_gpio_pins[pin_idx])==p1_v1_gpio_chip_ids[pin_idx] );
+    }
+  test_rpi_initialiser.test_rpi_board_version=2; 
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p1_gpio_pins;++pin_idx)
+    {
+      CHECK( p1_pin(p1_gpio_pins[pin_idx])==p1_v2_gpio_chip_ids[pin_idx] );
+    }
+}
+
+TEST_CASE( "Unit_tests/pin_id/bad_p1_pin_arguments_throws"
+         , "Bad and non-GPIO p1_pin pin numbers should throw std::invalid_argument"
+         )
+{
+  test_rpi_version_init setup;
+  test_rpi_initialiser.test_rpi_board_version=1;
+  REQUIRE_THROWS_AS(p1_pin(0),std::invalid_argument);
+  REQUIRE_THROWS_AS((p1_pin(p1_map_size)), std::invalid_argument);
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p1_non_gpio_pins;++pin_idx)
+    {
+      REQUIRE_THROWS_AS((p1_pin(p1_non_gpio_pins[pin_idx])),std::invalid_argument);
+    }
+  test_rpi_initialiser.test_rpi_board_version=2; 
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p1_non_gpio_pins;++pin_idx)
+    {
+      REQUIRE_THROWS_AS((p1_pin(p1_non_gpio_pins[pin_idx])),std::invalid_argument);
+    }
+  test_rpi_initialiser.test_rpi_board_version=pinout_versions+1;
+  REQUIRE_THROWS_AS((p1_pin(p1_gpio_pins[0])),std::invalid_argument);
+}
+
+TEST_CASE( "Unit_tests/pin_id/good_p5_pin_arguments_produce_expected_pin-ids"
+         , "p5_pin GPIO pin numbers should produce correct mapped pin_id value"
+         )
+{
+  test_rpi_version_init setup;
+  test_rpi_initialiser.test_rpi_board_version=2; 
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p5_gpio_pins;++pin_idx)
+    {
+      CHECK( p5_pin(p5_gpio_pins[pin_idx])==p5_v2_gpio_chip_ids[pin_idx] );
+    }
+}
+
+TEST_CASE( "Unit_tests/pin_id/bad_p5_pin_arguments_throws"
+         , "Bad and non-GPIO p5_pin pin numbers should throw std::invalid_argument"
+         )
+{
+  test_rpi_version_init setup;
+  test_rpi_initialiser.test_rpi_board_version=1;
+  REQUIRE_THROWS_AS((p5_pin(p1_map_size)), std::invalid_argument);
+  for (std::size_t pin_num=0; pin_num!=p5_map_size;++pin_num)
+    {
+      REQUIRE_THROWS_AS((p5_pin(pin_num)),std::invalid_argument);
+    }
+  test_rpi_initialiser.test_rpi_board_version=2; 
+  for (std::size_t pin_idx=0; pin_idx!=number_of_p5_v2_non_gpio_pins;++pin_idx)
+    {
+      REQUIRE_THROWS_AS((p5_pin(p5_v2_non_gpio_pins[pin_idx])),std::invalid_argument);
+    }
+  test_rpi_initialiser.test_rpi_board_version=pinout_versions+1;
+  REQUIRE_THROWS_AS((p5_pin(p5_gpio_pins[0])),std::invalid_argument);
 }

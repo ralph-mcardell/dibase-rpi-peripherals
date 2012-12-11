debb299e8992d5cca0eaf2dae032156097260081
Ralph McArdell <ralph@dibasepi1>
2012-12-11 17:48:42 +0000

    Updated documentation comments, added unit test cases.

--- src/pin_alloc.h
+++ src/pin_alloc.h
@@ -2,6 +2,51 @@
 /// @file pin_alloc.h 
 /// @brief Specification of GPIO pin allocation types.
 ///
+/// GPIO pins may be considered a resource access to which will usually be
+/// exclusive to one use in one process at a time with the possible exception
+/// of reading of GPIO pins in input mode. Many GPIO pins have alternate
+/// functions and the overloaded relationships between functions and GPIO
+/// pins could lead users to accidentially over-subscribe a pin by mistake.
+///
+/// Additionally, other processes might be running that use some GPIO pins.
+/// Unfortunately there is no fool-proof way to determine if this is so or
+/// prevent other processes from access GPIO pins supposedly in use by
+/// another process.
+///
+/// The pin allocation types try to tackle both the problem of requesting the
+/// same pin for more than one use within a process and prevent using GPIO
+/// pins that might be in use by other processes. The former relies on noting
+/// successfully allocated pins as in use and then as not in use when
+/// deallocated.
+///
+/// The more fuzzy problem of determining if a GPIO pin is being used by
+/// another process is only a partial solution. A GPIO pin is deemed to be in
+/// use if it has been previously exported in the Linux sys file system 
+/// (see /sys/classes/gpio). If is not then it is exported so any other
+/// processes using the same assumption will not try to also use the GPIO pin.
+/// As mentioned this is by no means a watertight solution and may be
+/// revisited in ther future.
+///
+/// To alloe testing the intra-process pin allocation logic as a unit the
+/// implementation is split into two parts:
+///   - a class template that implements the intra-process allocation logic
+///   - a separate allocation class that performs the inter-process sys file
+///     system chacking which is used as a type passed to the intra-process
+///     allocator template to provide the inter-process logic.
+///
+/// This allows the intra-process allocation logic to be tested on its own
+/// by mocking the inter-process allocator class. 
+///
+/// GPIO pin allocator types should provide the following three functions:
+///
+///   void allocate( pin_id pin )   : allocate a pin or throw a bad_pin_alloc
+///                                   exception if the pin is in use.
+///   void deallocate( pin_id pin ) : deallocate a previously allocated pin
+///                                   or thow a std::logic error if the pin is
+///                                   not allocated.
+///   bool is_in_use( pin_id pin )  : Returns true if the pin is in use at the
+///                                   time of the query, false if it is not.
+///
 /// @copyright Copyright (c) Dibase Limited 2012
 /// @author Ralph E. McArdell
 
@@ -16,8 +61,11 @@
 namespace dibase { namespace rpi {
   namespace peripherals
   {
+  /// @brief Exception type raised if a pin cannot be allcoated.
     struct bad_pin_alloc : std::runtime_error
     {
+    /// @brief Construct from an explainatory C string.
+    /// @param[in]  what  C string expaining allocation failure
       bad_pin_alloc( char const * what )
       : std::runtime_error( what )
       {}
@@ -25,9 +73,9 @@ namespace dibase { namespace rpi {
 
   /// @brief Passes (de-)allocation requests to other allocator & caches results
   ///
-  /// Class template taking another pin allocator type parameter which is
-  /// assumed to implement allocate, deallocate and is_in_use methods and whose
-  /// instances are default constructable.
+  /// Class template providing intra-process allocation logic and taking 
+  /// a pin allocator type parameter used to provide the inter-process pin
+  /// allocation logic.
   ///
   /// GPIO pin allocation and deallocation requests are first checked against
   /// cached results local to the pin_cache_allocator specialisation instance
@@ -37,10 +85,6 @@ namespace dibase { namespace rpi {
   /// updated as appropriate. The is_in_use query member function first checks
   /// the local cache and, if _not_ locally in use, passes the query onto the
   /// contained allocator.
-  ///
-  /// Note: the rationale behind splitting pin allocation into a local caching
-  /// part with a parametersable alternate part is to allow unit testing of the
-  /// caching functionality using simple mock contained allocator types.
   ///
   /// @param PIN_ALLOC_T  Type of pin allocator to pass requests onto if 
   ///                     cached results indicate so.
@@ -64,7 +108,7 @@ namespace dibase { namespace rpi {
     /// throw on allocation failure. If the call returns then the pin is marked
     /// as in use in the per-instance Pin Allocation Table.
     ///
-    /// @param[in] pin    GPIO pin id for the GPIO pin to allocate for use.
+    /// @param[in]  pin   GPIO pin id for the GPIO pin to allocate for use.
     /// @exception  bad_pin_alloc is raised if the requested pin is already in
     ///             use.
       void allocate( pin_id pin )
@@ -87,7 +131,7 @@ namespace dibase { namespace rpi {
     /// to also throw on deallocation failure. If the call returns then the pin
     /// is marked as free in the per-instance Pin Allocation Table.
     ///
-    /// @param[in] pin    GPIO pin id for the GPIO pin to deallocate from use.
+    /// @param[in]  pin   GPIO pin id for the GPIO pin to deallocate from use.
     /// @exception  std::logic_error is raised if the requested pin is not in
     ///             use.
       void deallocate( pin_id pin )
@@ -106,13 +150,13 @@ namespace dibase { namespace rpi {
     ///
     /// If the pin is marked in use in the per-instance Pin Allocation Table
     /// returns true, otherwise returns the value returned from passing the
-    /// pin_id to the conatined allocator's is_in_use member function.
+    /// pin_id to the contained allocator's is_in_use member function.
     ///
-    /// @param[in] pin    GPIO pin id of GPIO pin to see if it is in use.
+    /// @param[in]  pin     GPIO pin id of GPIO pin to see if it is in use.
     /// @return true if the pin is in use at this moment or false otherwise
       bool is_in_use( pin_id pin )
       {
-        return pin[pat] ? true : allocator.is_in_use( pin );
+        return pat[pin] ? true : allocator.is_in_use( pin );
       }
     };
   }
--- src/tests/pin_alloc_unittests.cpp
+++ src/tests/pin_alloc_unittests.cpp
@@ -14,12 +14,12 @@ struct mock_allocator
 {
   void allocate( pin_id /*pin*/ )
   {
-    if (in_use) throw std::bad_alloc();
+    if (in_use) throw std::domain_error("Oops - unexpected call to mock_allocator::allocate");
     in_use = true;
   }
   void deallocate( pin_id /*pin*/ )
   {
-    if (!in_use) throw std::logic_error("Cannot deallocate pin that is not in use!");
+    if (!in_use) throw std::domain_error("Oops - unexpected call to mock_allocator::deallocate");
     in_use = false;
   }
   bool is_in_use( pin_id /*pin*/ )
@@ -46,5 +46,62 @@ TEST_CASE( "Unit_tests/pin_cache_allocator/alloc_available_pin_checks_wrapped_al
   mock_allocator::in_use = false;
   pin_cache_allocator<mock_allocator> a;
   a.allocate(pin_id(2));
+  CHECK(mock_allocator::in_use==true);
+}
+
+TEST_CASE( "Unit_tests/pin_cache_allocator/alloc_unavailable_pin_throws"
+         , "When allocating an unavailable pin pin_cache_allocator throws a bad_pin_alloc exception"
+         )
+{
+  mock_allocator::in_use = false;
+  pin_cache_allocator<mock_allocator> a;
+  a.allocate(pin_id(3));
+  REQUIRE(a.is_in_use(pin_id(3))==true);
+  REQUIRE_THROWS_AS(a.allocate(pin_id(3)), bad_pin_alloc);
+}
+
+TEST_CASE( "Unit_tests/pin_cache_allocator/alloc_used_elsewhere_pin_throws_not_locally_in_use"
+         , "When allocating an pin used elsewhere pin_cache_allocator does not mark pin as in use locally"
+         )
+{
+  mock_allocator::in_use = true;
+  pin_cache_allocator<mock_allocator> a;
+  REQUIRE_THROWS_AS(a.allocate(pin_id(4)), std::domain_error);
+  REQUIRE(mock_allocator::in_use==true);
+//2nd alloc. request only passed to mock_allocator if pin still marked free by a
+  REQUIRE_THROWS_AS(a.allocate(pin_id(4)), std::domain_error);
+}
+
+TEST_CASE( "Unit_tests/pin_cache_allocator/alloc_pins_independent"
+         , "Allocating one pin does not interfere with allocating another"
+         )
+{
+  mock_allocator::in_use = false;
+  pin_cache_allocator<mock_allocator> a;
+  a.allocate(pin_id(5));
+  REQUIRE(mock_allocator::in_use==true);
+// 2nd pin alloc. request only passed to mock_allocator if pin marked free by a
+  REQUIRE_THROWS_AS(a.allocate(pin_id(6)), std::domain_error);
+}
+
+TEST_CASE( "Unit_tests/pin_cache_allocator/alloc_min_pin_id_OK"
+         , "Allocating minimum supported pin_id works OK"
+         )
+{
+  mock_allocator::in_use = false;
+  pin_cache_allocator<mock_allocator> a;
+  a.allocate(pin_id(pin_id::min_id));
+  CHECK(a.is_in_use(pin_id(pin_id::min_id))==true);
+  CHECK(mock_allocator::in_use==true);
+}
+
+TEST_CASE( "Unit_tests/pin_cache_allocator/alloc_max_pin_id_OK"
+         , "Allocating maximum supported pin_id works OK"
+         )
+{
+  mock_allocator::in_use = false;
+  pin_cache_allocator<mock_allocator> a;
+  a.allocate(pin_id(pin_id::max_id));
+  CHECK(a.is_in_use(pin_id(pin_id::max_id))==true);
   CHECK(mock_allocator::in_use==true);
 }

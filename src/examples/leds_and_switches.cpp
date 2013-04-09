3e3ba9fb2c0be615018fe1ae86a7aa2148f7662b
Ralph McArdell <ralph@dibasepi1>
2013-04-09 13:42:48 +0100

    Moved switch counter logic to separate function executed on its own thread while main (thread) waits for user to press enter on std::cin

--- src/examples/leds_and_switches.cpp
+++ src/examples/leds_and_switches.cpp
@@ -12,33 +12,61 @@
 
 #include <exception>    // for std::exception
 #include <iostream>     // for std IO stream objects
-#include <unistd.h>     // for sleep
+#include <unistd.h>     // for usleep
+#include <thread>
 
 using namespace dibase::rpi::peripherals;
 
-int main()
+bool g_running{ true };
+
+void count_switch_presses()
 {
   try
     {
-      opin led0(gpio_gen6);
-      opin led1(gpio_gen5);
-      opin led2(gpio_gen4);
-      opin led3(gpio_gen3);
+      opin led0{gpio_gen6};
+      opin led1{gpio_gen5};
+      opin led2{gpio_gen4};
+      opin led3{gpio_gen3};
 
-      ipin switch0(gpio_gen2);
+      ipin switch0{gpio_gen2};
 
-      for (unsigned p = 0; p < 17; ++p) 
+      unsigned const switch_delay_ms{70};
+      unsigned const switch_delay_us{switch_delay_ms*1000};
+      
+      unsigned count{0};
+      while (g_running) 
         {
-          led0.put((p&1));
-          led1.put((p&2));
-          led2.put((p&4));
-          led3.put((p&8));
-          std::cout << "Switch0 state: " << switch0.get() << std::endl;
-          sleep(1);
+          while (!switch0.get() && g_running)
+            usleep(switch_delay_us);
+          while (switch0.get() && g_running)
+            usleep(switch_delay_us);
+          if (g_running)
+            {
+              ++count;
+              led0.put((count&1));
+              led1.put((count&2));
+              led2.put((count&4));
+              led3.put((count&8));
+              std::cout << "Count: " << count << std::endl;
+            }
         }
+      led0.put(false);
+      led1.put(false);
+      led2.put(false);
+      led3.put(false);
     }
   catch ( std::exception & e )
     {
       std::cerr << "A problem occured. Description: " << e.what() << "\n";
     }
+}
+
+int main()
+{
+  std::cout << "Press enter to quit....\n";
+  std::thread counter{ count_switch_presses };
+  std::string dummy;
+  std::getline(std::cin, dummy);
+  g_running = false;
+  counter.join();
 }

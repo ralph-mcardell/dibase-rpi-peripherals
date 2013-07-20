bdd5a1b62acc513dcce37e17a71864b2ed92a515
Ralph McArdell <ralph@dibasepi1>
2013-07-20 16:50:39 +0100

    Changed so open_collector example will continue doing its thing until users presses enter on console and to have the open-collector output track the value from an input GPIO pin.

--- src/examples/open-collector.cpp
+++ src/examples/open-collector.cpp
@@ -12,26 +12,48 @@
 
 #include <exception>    // for std::exception
 #include <iostream>     // for std IO stream objects
-#include <unistd.h>     // for sleep
+#include <chrono>
+#include <thread>
 
 using namespace dibase::rpi::peripherals;
+bool g_running{ true };  ///< Global flag used to communicate quit request
 
-int main()
+void switch_oc_output_on_input()
 {
   try
     {
-      opin gpio_clk_out(gpio_gclk);
+      opin gpio_clk_out(gpio_gclk); // Gertboard J2 GP4 -- connect to RLYn
+      ipin in{spi_ce0_n};           // Gertboard J2 GP8 -- connect to i/p device
 
-      for (unsigned p = 0; p < 10; ++p) 
+  // Open collector state of RLYn follows that of GP8 input value
+  // track changes of input state until not running...
+      bool in_value{in.get()};
+      gpio_clk_out.put(in_value);
+      unsigned const input_change_wait_ms{50};
+      while (g_running)
         {
-          gpio_clk_out.put(true);
-          sleep(1);
-          gpio_clk_out.put(false);
-          sleep(1);
+          while (in.get()==in_value && g_running)
+            std::this_thread::sleep_for
+                              (std::chrono::milliseconds(input_change_wait_ms));
+          if (g_running)
+            {
+              in_value = ! in_value;
+              gpio_clk_out.put(in_value);
+            }
         }
     }
   catch ( std::exception & e )
     {
       std::cerr << "A problem occured. Description: " << e.what() << "\n";
     }
+}
+
+int main()
+{
+  std::cout << "Press enter to quit....\n";
+  std::thread worker{ switch_oc_output_on_input };
+  std::string dummy;
+  std::getline(std::cin, dummy);
+  g_running = false;
+  worker.join();
 }

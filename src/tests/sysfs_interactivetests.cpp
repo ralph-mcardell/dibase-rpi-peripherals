5c84ffe6865de3816cc54cdb27dd552cdc227ec9
Ralph McArdell <ralph@dibasepi1>
2013-07-22 20:53:51 +0100

    Added sysfs edge event tests including new interactive tests

---
+++ src/tests/sysfs_interactivetests.cpp
@@ -0,0 +1,124 @@
+// Project: Raspberry Pi BCM2708 / BCM2835 peripherals C++ library
+/// @file sysfs_interactivetests.cpp 
+/// @brief Interactive tests for sys filesystem utilities.
+///
+/// @copyright Copyright (c) Dibase Limited 2013
+/// @author Ralph E. McArdell
+
+#include "catch.hpp"
+#include "sysfs.h"
+#include "interactivetests_config.h"
+#include <iostream>
+
+using namespace dibase::rpi::peripherals;
+using namespace dibase::rpi::peripherals::internal;
+
+unsigned const select_wait_time_secs{1U};
+unsigned const max_wait_time_secs{5U};
+
+TEST_CASE( "Interactive_tests/sysfs/0000/initial edge event notifications OK"
+         , "Setup for any edge events triggers initial resturn from select"
+         )
+{
+  export_pin(test::in_pin_id);
+  REQUIRE(is_exported(test::in_pin_id)==true);
+  auto fd(open_ipin_for_edge_events(test::in_pin_id, edge_event_mode::rising));
+  REQUIRE(fd!=-1);
+  int select_retval{0};
+  timeval tv;
+  tv.tv_sec = select_wait_time_secs; tv.tv_usec = 0U;
+  fd_set xfds;
+  FD_ZERO(&xfds);
+  FD_SET(fd, &xfds);
+  select_retval = ::select(fd+1, NULL, NULL, &xfds, &tv);
+  CHECK(select_retval==1);
+  if (select_retval)
+    {
+      bool fd_in_xfds_isset{FD_ISSET(fd,&xfds)};
+      CHECK(fd_in_xfds_isset);
+    }
+  CHECK(close_ipin_for_edge_events(fd)==true);
+  unexport_pin(test::in_pin_id);
+  CHECK(is_exported(test::in_pin_id)==false);
+}
+
+ void wait_on_select(edge_event_mode mode)
+ {
+  export_pin(test::in_pin_id);
+  REQUIRE(is_exported(test::in_pin_id)==true);
+  auto fd(open_ipin_for_edge_events(test::in_pin_id, mode));
+  REQUIRE(fd!=-1);
+  int select_retval{0};
+  bool initialisation_notification{true};
+  for ( unsigned wt{0U}; wt<max_wait_time_secs; wt += select_wait_time_secs)
+    {
+      timeval tv;
+      tv.tv_sec = select_wait_time_secs; tv.tv_usec = 0U;
+      fd_set xfds;
+      FD_ZERO(&xfds);
+      FD_SET(fd, &xfds);
+      select_retval = ::select(fd+1, NULL, NULL, &xfds, &tv);
+      CHECK(select_retval!=-1);
+      if (select_retval==-1) break;
+      if (select_retval)
+        {
+          bool fd_in_xfds_isset{FD_ISSET(fd,&xfds)};
+          CHECK(fd_in_xfds_isset);
+          if (initialisation_notification)
+            {
+              initialisation_notification = false;
+              REQUIRE(lseek(fd,0,SEEK_SET)==0);
+              char v{'\0'};
+              REQUIRE(read(fd,&v,1)==1);
+            }
+          else
+            {
+              break;
+            }
+        }
+      std::cout << "\b" << max_wait_time_secs - wt;
+      std::cout.flush();
+    }
+  CHECK(select_retval==1);
+  CHECK(close_ipin_for_edge_events(fd)==true);
+  unexport_pin(test::in_pin_id);
+  CHECK(is_exported(test::in_pin_id)==false);
+}
+
+TEST_CASE( "Interactive_tests/sysfs/0010/rising edge event notifications OK"
+         , "Can successfully wait for rising edge events only"
+         )
+{
+  std::cout << "sysfs: In pin rising edge notification test:\n"
+               "Connect pin BCM2835 GPIO" << test::in_pin_id 
+            << " to allow state changing between high voltage and ground "
+               "(switch+resistors etc.)\n\n";
+  std::cout << "Set BCM2835 GPIO" << test::in_pin_id << " low then high... ";
+  wait_on_select(edge_event_mode::rising);
+}
+
+TEST_CASE( "Interactive_tests/sysfs/0020/falling edge event notifications OK"
+         , "Can successfully wait for falling edge events only"
+         )
+{
+  std::cout << "\n\n\nsysfs: In pin falling edge notification test:\n"
+               "Connect pin BCM2835 GPIO" << test::in_pin_id 
+            << " to allow state changing between high voltage and ground "
+               "(switch+resistors etc.)\n\n";
+  std::cout << "Set BCM2835 GPIO" << test::in_pin_id << " high then low... ";
+  wait_on_select(edge_event_mode::falling);
+}
+
+TEST_CASE( "Interactive_tests/sysfs/0030/both edge event notifications OK"
+         , "Can successfully wait for either edge event"
+         )
+{
+  std::cout << "\n\n\nsysfs: In pin both edges notification test:\n"
+               "Connect pin BCM2835 GPIO" << test::in_pin_id 
+            << " to allow state changing between high voltage and ground "
+               "(switch+resistors etc.)\n\n";
+  std::cout << "Set BCM2835 GPIO" << test::in_pin_id << " low then high... ";
+  wait_on_select(edge_event_mode::both);
+  std::cout << "\nSet BCM2835 GPIO" << test::in_pin_id << " high then low... ";
+  wait_on_select(edge_event_mode::both);
+}
--- src/tests/sysfs_systemtests.cpp
+++ src/tests/sysfs_systemtests.cpp
@@ -7,6 +7,7 @@
 
 #include "catch.hpp"
 #include "sysfs.h"
+#include <stdio.h>
 
 using namespace dibase::rpi::peripherals;
 using namespace dibase::rpi::peripherals::internal;
@@ -14,14 +15,14 @@ using namespace dibase::rpi::peripherals::internal;
 // Change if P1 GPIO_GEN0 is in use on your system...
 pin_id const available_pin_id{17}; // P1 pin GPIO_GEN0
 
-TEST_CASE( "System_tests/000/sysfs/is_exported_initially_reports_free"
+TEST_CASE( "System_tests/0000/sysfs/is_exported_initially_reports_free"
          , "The available_pin_id is initially not exported"
          )
 {
   REQUIRE(is_exported(available_pin_id)==false);
 }
 
-TEST_CASE( "System_tests/001/sysfs/export_pin_exported_unexported_not"
+TEST_CASE( "System_tests/0010/sysfs/export_pin_exported_unexported_not"
          , "Exported pin is exported, then when unexported it is not exported."
          )
 {
@@ -31,7 +32,7 @@ TEST_CASE( "System_tests/001/sysfs/export_pin_exported_unexported_not"
   CHECK(is_exported(available_pin_id)==false);
 }
 
-TEST_CASE( "System_tests/002/sysfs/exporting_exported_pin_OK"
+TEST_CASE( "System_tests/0020/sysfs/exporting_exported_pin_OK"
          , "Re-exporting a pin WORKS - check is_exported to check if this is"
            " not what is required."
          )
@@ -44,12 +45,66 @@ TEST_CASE( "System_tests/002/sysfs/exporting_exported_pin_OK"
   CHECK(is_exported(available_pin_id)==false);
 }
 
-TEST_CASE( "System_tests/003/sysfs/unexporting an unexported pin OK"
+TEST_CASE( "System_tests/0030/sysfs/unexporting an unexported pin OK"
          , "Unexporting a pin that is not exported OK - use is_exported to"
            " check if this is not what is required"
          )
 {
   REQUIRE(is_exported(available_pin_id)==false);
   CHECK(unexport_pin(available_pin_id)==true);
+  CHECK(is_exported(available_pin_id)==false);
+}
+
+TEST_CASE( "System_tests/0040/sysfs/opening unexported pin for events fails"
+         , "A pin must be exported before it can be opened for edge event IO "
+           "multiplexing use."
+         )
+{
+  REQUIRE(is_exported(available_pin_id)==false);
+  REQUIRE_THROWS_AS( open_ipin_for_edge_events
+                      (available_pin_id, edge_event_mode::both)
+                   , std::runtime_error
+                   );
+}
+
+TEST_CASE( "System_tests/0050/sysfs/opening pin for events with bad mode fails"
+         , "Passing a bad edge_event_mode value to open_ipin_for_edge_events "
+           "fails"
+         )
+{
+  export_pin(available_pin_id);
+  REQUIRE(is_exported(available_pin_id)==true);
+  REQUIRE_THROWS_AS( open_ipin_for_edge_events
+                      (available_pin_id, static_cast<edge_event_mode>(1234))
+                   , std::invalid_argument
+                   );
+  unexport_pin(available_pin_id);
+  CHECK(is_exported(available_pin_id)==false);
+}
+
+TEST_CASE( "System_tests/0060/sysfs/closing non-open pin for events fd fails"
+         , "Closing a pin for edge event file dscriptor that is not open fails"
+         )
+{
+  CHECK(close_ipin_for_edge_events(-1)==false);
+  FILE* ftmp{tmpfile()};
+  REQUIRE(ftmp!=0);
+  auto fd(fileno(ftmp));
+  REQUIRE(fd>STDERR_FILENO);
+  REQUIRE(fclose(ftmp)==0);
+  CHECK(close_ipin_for_edge_events(fd)==false);
+}
+
+TEST_CASE( "System_tests/0070/sysfs/open/close exported pin for events OK"
+         , "Passing a valid argument values to open_ipin_for_edge_events "
+           "is sucessful"
+         )
+{
+  export_pin(available_pin_id);
+  REQUIRE(is_exported(available_pin_id)==true);
+  auto fd(open_ipin_for_edge_events(available_pin_id, edge_event_mode::both));
+  CHECK(fd!=-1);
+  CHECK(close_ipin_for_edge_events(fd)==true);
+  unexport_pin(available_pin_id);
   CHECK(is_exported(available_pin_id)==false);
 }

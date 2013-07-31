c296056d21ec095b172c1616b0b1739cb7888df3
Ralph McArdell <ralph@dibasepi1>
2013-07-31 18:02:57 +0100

    Added single bit clock control register read & modify operations

--- src/clock_registers.h
+++ src/clock_registers.h
@@ -20,6 +20,16 @@
 namespace dibase { namespace rpi {
   namespace peripherals
   {
+  /// @brief Override clock busy enumerations
+  /// Many operations are best not performed if a clock is busy and 
+  /// interface functions usually do not allow such operations to proceed
+  /// is a clock is busy by default but allow forcing the issue if the
+  /// caller specifies busy_override::yes.
+    enum class busy_override
+    { no      ///< Do not override do-not-do-if-clock-busy advise
+    , yes     ///< Override do-not-do-if-clock-busy advise
+    };
+
   /// @brief Clock manager control registers record for a single clock
   ///
   /// Each clock has two control registers: 
@@ -29,8 +39,82 @@ namespace dibase { namespace rpi {
   /// divisor respectfully.
     struct clock_record
     {
+    private:
+      enum
+      { ctrl_enab_mask = (1U<<4)
+      , ctrl_kill_mask = (1U<<5)
+      , ctrl_busy_mask = (1U<<7)
+      , ctrl_flip_mask = (1U<<8)
+
+      /// @brief Magic value: see BCM2835 peripherals manual tables 6-34 & 35.
+      , password = 0x5A000000U
+      };
+
+    public:
       register_t control; ///< Clock control (XX_CTL) register
       register_t divisor; ///< Clock fequency divisor (XX_DIV) register
+
+    /// @brief Return ststus of control register BUSY flag.
+    /// @returns true if control register BUSY bit set, false if not.
+      bool is_busy() volatile { return control&ctrl_busy_mask; }
+
+    /// @brief Returns value of ENAB control register bit
+    /// @returns true if ENAB control bit set or false if it is not.
+      bool get_enable() volatile { return control&ctrl_enab_mask; }
+
+    /// @brief Returns value of KILL control register bit
+    /// @returns true if KILL control bit set or false if it is not.
+      bool get_kill() volatile { return control&ctrl_kill_mask; }
+
+    /// @brief Returns value of FLIP control register bit
+    /// @returns true if FLIP control bit set or false if it is not.
+      bool get_flip() volatile { return control&ctrl_flip_mask; }
+
+    /// @brief Set the value of ENAB control register bit
+    /// Will not perform operation if clock is busy and force is not
+    /// busy_override::yes.
+    /// @param state State to set ENAB bit to: true->1, false->0
+    /// @param force Specifies whether to allow operation if clock is busy
+    /// @returns  true if operation performed, false if it was not performed
+    ///           because clock was busy.
+      bool set_enable(bool state, busy_override force=busy_override::no)
+      volatile
+      { 
+        if (force==busy_override::no && is_busy())
+          {
+            return false;
+          }
+        control = password|(state ? control|ctrl_enab_mask
+                                  : control&(~ctrl_enab_mask));
+        return true;
+      }
+
+    /// @brief Set the value of KILL control register bit
+    /// @param state State to set KILL bit to: true->1, false->0
+      void set_kill(bool state) volatile
+      { 
+        control = password|(state ? control|ctrl_kill_mask
+                                  : control&(~ctrl_kill_mask));
+      }
+
+    /// @brief Set the value of FLIP control register bit
+    /// Will not perform operation if clock is busy and force is not
+    /// busy_override::yes.
+    /// @param state State to set FLIP bit to: true->1, false->0
+    /// @param force Specifies whether to allow operation if clock is busy
+    /// @returns  true if operation performed, false if it was not performed
+    ///           because clock was busy.
+      bool set_flip(bool state, busy_override force=busy_override::no)
+      volatile
+      { 
+        if (force==busy_override::no && is_busy())
+          {
+            return false;
+          }
+        control = password|(state ? control|ctrl_flip_mask
+                                  : control&(~ctrl_flip_mask));
+        return true;
+      }
     };
 
     struct clock_registers;
@@ -58,9 +142,6 @@ namespace dibase { namespace rpi {
       
     /// @brief 32-bit register gap between GP clocks end & PWN clocks start
       , gp_pwm_gap=pwm_offset-gp_offset-(num_gp_clks*regs_per_clk)
-
-      /// @brief Magic value: see BCM2835 peripherals manual tables 6-34 & 35.
-      , password = 0x5A000000
       };
     public:
     /// @brief Physical address of start of BCM2835 clock control registers
@@ -74,6 +155,68 @@ namespace dibase { namespace rpi {
 
       register_t reserved_do_not_use_1[gp_pwm_gap];///< Reserved, currently unused
       clock_record pwm_clk; ///< PWM clock
+
+    /// @brief Return status of control register BUSY flag for specified clock.
+    /// @param clk  Clock id of clock to return busy status of
+    /// @returns true if control register BUSY bit set, false if not.
+      bool is_busy(clock_id clk) volatile { return (this->*clk).is_busy(); }
+
+    /// @brief Return value of control register ENAB bit for specified clock.
+    /// @param clk  Clock id of clock to return enable bit value of
+    /// @returns true if control register ENAB bit set, false if not.
+      bool get_enable(clock_id clk) volatile {return (this->*clk).get_enable();}
+
+    /// @brief Return value of control register KILL bit for specified clock.
+    /// @param clk  Clock id of clock to return kill bit value of
+    /// @returns true if control register KILL bit set, false if not.
+      bool get_kill(clock_id clk) volatile { return (this->*clk).get_kill(); }
+
+    /// @brief Return value of control register FLIP bit for specified clock.
+    /// @param clk  Clock id of clock to return flip bit value of
+    /// @returns true if control register FLIP bit set, false if not.
+      bool get_flip(clock_id clk) volatile { return (this->*clk).get_flip(); }
+
+    /// @brief Set the value of ENAB control register bit
+    /// Will not perform operation if clock is busy and force is not
+    /// busy_override::yes.
+    /// @param clk  Clock id of clock to modify enable bit value of
+    /// @param state State to set ENAB bit to: true->1, false->0
+    /// @param force Specifies whether to allow operation if clock is busy
+    /// @returns  true if operation performed, false if it was not performed
+    ///           because clock was busy.
+      bool set_enable
+      ( clock_id clk
+      , bool state
+      , busy_override force=busy_override::no
+      ) volatile 
+      {
+        return (this->*clk).set_enable(state, force);
+      }
+
+    /// @brief Set the value of KILL control register bit
+    /// @param clk  Clock id of clock to modify kill bit value of
+    /// @param state State to set KILL bit to: true->1, false->0
+      void set_kill(clock_id clk, bool state) volatile 
+      {
+        (this->*clk).set_kill(state);
+      }
+
+    /// @brief Set the value of FLIP control register bit
+    /// Will not perform operation if clock is busy and force is not
+    /// busy_override::yes.
+    /// @param clk  Clock id of clock to modify flip bit value of
+    /// @param state State to set FLIP bit to: true->1, false->0
+    /// @param force Specifies whether to allow operation if clock is busy
+    /// @returns  true if operation performed, false if it was not performed
+    ///           because clock was busy.
+      bool set_flip
+      ( clock_id clk
+      , bool state
+      , busy_override force=busy_override::no
+      ) volatile 
+      {
+        return (this->*clk).set_flip(state, force);
+      }
     };
 
     clock_id const gp0_clk_id{&clock_registers::gp0_clk};
--- src/tests/clock_registers_unittests.cpp
+++ src/tests/clock_registers_unittests.cpp
@@ -22,6 +22,125 @@ using namespace dibase::rpi::peripherals;
 typedef uint32_t RegisterType;
 typedef unsigned char Byte;
 
+TEST_CASE( "Unit-tests/clock_record/0000/is_busy"
+         , "Clock control BUSY bit read and returned correctly by is_busy"
+         )
+{
+  clock_record cr{128U,0}; // BUSY flag is control register bit 7
+  CHECK(cr.is_busy());
+  cr.control &= ~128U;
+  CHECK_FALSE(cr.is_busy());
+}
+
+TEST_CASE( "Unit-tests/clock_record/0010/get_enable"
+         , "Clock control ENAB bit read and returned correctly by get_enable"
+         )
+{
+  clock_record cr{16U,0}; // ENAB bit is control register bit 4
+  CHECK(cr.get_enable());
+  cr.control &= ~16U;
+  CHECK_FALSE(cr.get_enable());
+}
+
+TEST_CASE( "Unit-tests/clock_record/0020/get_kill"
+         , "Clock control KILL bit read and returned correctly by get_kill"
+         )
+{
+  clock_record cr{32U,0}; // KILL bit is control register bit 5
+  CHECK(cr.get_kill());
+  cr.control &= ~32U;
+  CHECK_FALSE(cr.get_kill());
+}
+
+TEST_CASE( "Unit-tests/clock_record/0030/get_flip"
+         , "Clock control FLIP bit read and returned correctly by get_flip"
+         )
+{
+  clock_record cr{256U,0}; // FLIP bit is control register bit 8
+  CHECK(cr.get_flip());
+  cr.control &= ~256U;
+  CHECK_FALSE(cr.get_flip());
+}
+
+TEST_CASE( "Unit-tests/clock_record/0040/set_enable"
+         , "Clock control ENAB bit written correctly by set_enable"
+         )
+{
+  clock_record cr{0,0};
+  CHECK(cr.set_enable(true));
+  CHECK(cr.get_enable());
+  CHECK(cr.control==0x5a000010U);//0x5a000000 is write password; 0x10 is ENAB bit
+  CHECK(cr.set_enable(false, busy_override::yes));
+  CHECK_FALSE(cr.get_enable());
+  CHECK(cr.control==0x5a000000U);//0x5a000000 is write password; ENAB bit is off
+}
+
+TEST_CASE( "Unit-tests/clock_record/0050/set_kill"
+         , "Clock control KILL bit written correctly by set_kill"
+         )
+{
+  clock_record cr{0,0};
+  cr.set_kill(true);
+  CHECK(cr.get_kill());
+  CHECK(cr.control==0x5a000020U);//0x5a000000 is write password; 0x20 is KILL bit
+  cr.set_kill(false);
+  CHECK_FALSE(cr.get_kill());
+  CHECK(cr.control==0x5a000000U);//0x5a000000 is write password; KILL bit is off
+}
+
+TEST_CASE( "Unit-tests/clock_record/0060/set_flip"
+         , "Clock control FLIP bit written correctly by set_flip"
+         )
+{
+  clock_record cr{0,0};
+  CHECK(cr.set_flip(true));
+  CHECK(cr.get_flip());
+  CHECK(cr.control==0x5a000100U);//0x5a000000 is write password; 0x100 is FLIP bit
+  CHECK(cr.set_flip(false, busy_override::yes));
+  CHECK_FALSE(cr.get_flip());
+  CHECK(cr.control==0x5a000000U);//0x5a000000 is write password; FLIP bit is off
+}
+
+TEST_CASE( "Unit-tests/clock_record/0060/set_enable for busy clocks"
+         , "Check set_anable behaviour for busy clocks is as expected"
+         )
+{
+  clock_record cr{128U,0}; // control bit 7 is BUSY flag.
+  CHECK_FALSE(cr.set_enable(true));
+  CHECK_FALSE(cr.get_enable());
+  CHECK(cr.control==128U);// BUSY and not forced: no write occured
+  CHECK_FALSE(cr.set_enable(false));
+  CHECK_FALSE(cr.get_enable());
+  CHECK(cr.control==128U);// BUSY and not forced: no write occured
+
+  CHECK(cr.set_enable(true, busy_override::yes));
+  CHECK(cr.get_enable());
+  CHECK(cr.control==0x5a000090U);//0x5a000000=write password; 0x90=ENAB|BUSY
+  CHECK(cr.set_enable(false, busy_override::yes));
+  CHECK_FALSE(cr.get_enable());
+  CHECK(cr.control==0x5a000080U);//0x5a000000=write password; 0x80=BUSY, ENAB off
+}
+
+TEST_CASE( "Unit-tests/clock_record/0060/set_flip for busy clocks"
+         , "Check set_flip bit behaviour for busy clocks is as expected"
+         )
+{
+  clock_record cr{128U,0};
+  CHECK_FALSE(cr.set_flip(true));
+  CHECK_FALSE(cr.get_flip());
+  CHECK(cr.control==128U);// BUSY and not forced: no write occured
+  CHECK_FALSE(cr.set_flip(false));
+  CHECK_FALSE(cr.get_flip());
+  CHECK(cr.control==128U);// BUSY and not forced: no write occured
+
+  CHECK(cr.set_flip(true, busy_override::yes));
+  CHECK(cr.get_flip());
+  CHECK(cr.control==0x5a000180U);//0x5a000000=write password; 0x180=FLIP|BUSY
+  CHECK(cr.set_flip(false, busy_override::yes));
+  CHECK_FALSE(cr.get_flip());
+  CHECK(cr.control==0x5a000080U);//0x5a000000=write password; 0x80=BUSY, FLIP off
+}
+
 enum RegisterOffsets // Byte offsets
 { PWM_CTRL_OFFSET=40*4, PWM_DIV_OFFSET=41*4  // NB: NOT HEX values!!
 , GP0_CTRL_OFFSET=0x70, GP0_DIV_OFFSET=0x74
@@ -29,7 +148,7 @@ enum RegisterOffsets // Byte offsets
 , GP2_CTRL_OFFSET=0x80, GP2_DIV_OFFSET=0x84
 };
 
-TEST_CASE( "Unit-tests/clock_registers/field offsets"
+TEST_CASE( "Unit-tests/clock_registers/0000/field offsets"
          , "Clock control and divisor fields should have the expected offsets"
          )
 {
@@ -60,7 +179,7 @@ TEST_CASE( "Unit-tests/clock_registers/field offsets"
   CHECK( reinterpret_cast<RegisterType&>(reg_base_addr[GP2_DIV_OFFSET])==GP2_DIV_OFFSET );
  }
  
-TEST_CASE( "Unit-tests/clock_registers/pre-defined clock_id constants correct"
+TEST_CASE( "Unit-tests/clock_registers/0010/pre-defined clock_id constants correct"
          , "Defined clock id member pointers should refer to the expected fields"
          )
 {
@@ -91,3 +210,284 @@ TEST_CASE( "Unit-tests/clock_registers/pre-defined clock_id constants correct"
   CHECK( reinterpret_cast<RegisterType&>(reg_base_addr[GP2_DIV_OFFSET])==GP2_DIV_OFFSET );
  }
  
+ TEST_CASE( "Unit-tests/clock_registers/0020/is_busy"
+         , "Control BUSY bit read and returned OK by is_busy for given clock"
+         )
+{
+  clock_registers clk_regs;
+// initially start with all bytes of gpio_regs set to 0x00:
+  std::memset(&clk_regs, 0x00, sizeof(clk_regs));
+
+  clk_regs.pwm_clk.control |= 128U;// BUSY flag is control register bit 7
+  CHECK(clk_regs.is_busy(pwm_clk_id));
+  clk_regs.pwm_clk.control &= ~128U;
+  CHECK_FALSE(clk_regs.is_busy(pwm_clk_id));
+
+  clk_regs.gp0_clk.control |= 128U;
+  CHECK(clk_regs.is_busy(gp0_clk_id));
+  clk_regs.gp0_clk.control &= ~128U;
+  CHECK_FALSE(clk_regs.is_busy(gp0_clk_id));
+
+  clk_regs.gp1_clk.control |= 128U;
+  CHECK(clk_regs.is_busy(gp1_clk_id));
+  clk_regs.gp1_clk.control &= ~128U;
+  CHECK_FALSE(clk_regs.is_busy(gp1_clk_id));
+
+  clk_regs.gp2_clk.control |= 128U;
+  CHECK(clk_regs.is_busy(gp2_clk_id));
+  clk_regs.gp2_clk.control &= ~128U;
+  CHECK_FALSE(clk_regs.is_busy(gp2_clk_id));
+}
+
+ TEST_CASE( "Unit-tests/clock_registers/0030/get_enable"
+         , "Control ENAB bit read and returned OK by get_enable for given clock"
+         )
+{
+  clock_registers clk_regs;
+// initially start with all bytes of gpio_regs set to 0x00:
+  std::memset(&clk_regs, 0x00, sizeof(clk_regs));
+
+  clk_regs.pwm_clk.control |= 16U;// ENAB bit is control register bit 4
+  CHECK(clk_regs.get_enable(pwm_clk_id));
+  clk_regs.pwm_clk.control &= ~16U;
+  CHECK_FALSE(clk_regs.get_enable(pwm_clk_id));
+
+  clk_regs.gp0_clk.control |= 16U;
+  CHECK(clk_regs.get_enable(gp0_clk_id));
+  clk_regs.gp0_clk.control &= ~16U;
+  CHECK_FALSE(clk_regs.get_enable(gp0_clk_id));
+
+  clk_regs.gp1_clk.control |= 16U;
+  CHECK(clk_regs.get_enable(gp1_clk_id));
+  clk_regs.gp1_clk.control &= ~16U;
+  CHECK_FALSE(clk_regs.get_enable(gp1_clk_id));
+
+  clk_regs.gp2_clk.control |= 16U;
+  CHECK(clk_regs.get_enable(gp2_clk_id));
+  clk_regs.gp2_clk.control &= ~16U;
+  CHECK_FALSE(clk_regs.get_enable(gp2_clk_id));
+}
+
+ TEST_CASE( "Unit-tests/clock_registers/0040/get_kill"
+         , "Control KILL bit read and returned OK by get_kill for given clock"
+         )
+{
+  clock_registers clk_regs;
+// initially start with all bytes of gpio_regs set to 0x00:
+  std::memset(&clk_regs, 0x00, sizeof(clk_regs));
+
+  clk_regs.pwm_clk.control |= 32U;// KILL bit is control register bit 5
+  CHECK(clk_regs.get_kill(pwm_clk_id));
+  clk_regs.pwm_clk.control &= ~32U;
+  CHECK_FALSE(clk_regs.get_kill(pwm_clk_id));
+
+  clk_regs.gp0_clk.control |= 32U;
+  CHECK(clk_regs.get_kill(gp0_clk_id));
+  clk_regs.gp0_clk.control &= ~32U;
+  CHECK_FALSE(clk_regs.get_kill(gp0_clk_id));
+ 
+  clk_regs.gp1_clk.control |= 32U;
+  CHECK(clk_regs.get_kill(gp1_clk_id));
+  clk_regs.gp1_clk.control &= ~32U;
+  CHECK_FALSE(clk_regs.get_kill(gp1_clk_id));
+
+  clk_regs.gp2_clk.control |= 32U;
+  CHECK(clk_regs.get_kill(gp2_clk_id));
+  clk_regs.gp2_clk.control &= ~32U;
+  CHECK_FALSE(clk_regs.get_kill(gp2_clk_id));
+}
+
+ TEST_CASE( "Unit-tests/clock_registers/0050/get_flip"
+         , "Control FLIP bit read and returned OK by get_flip for given clock"
+         )
+{
+  clock_registers clk_regs;
+// initially start with all bytes of gpio_regs set to 0x00:
+  std::memset(&clk_regs, 0x00, sizeof(clk_regs));
+
+  clk_regs.pwm_clk.control |= 256U;// FLIP bit is control register bit 8
+  CHECK(clk_regs.get_flip(pwm_clk_id));
+  clk_regs.pwm_clk.control &= ~256U;
+  CHECK_FALSE(clk_regs.get_flip(pwm_clk_id));
+
+  clk_regs.gp0_clk.control |= 256U;
+  CHECK(clk_regs.get_flip(gp0_clk_id));
+  clk_regs.gp0_clk.control &= ~256U;
+  CHECK_FALSE(clk_regs.get_flip(gp0_clk_id));
+
+  clk_regs.gp1_clk.control |= 256U;
+  CHECK(clk_regs.get_flip(gp1_clk_id));
+  clk_regs.gp1_clk.control &= ~256U;
+  CHECK_FALSE(clk_regs.get_flip(gp1_clk_id));
+
+  clk_regs.gp2_clk.control |= 256U;
+  CHECK(clk_regs.get_flip(gp2_clk_id));
+  clk_regs.gp2_clk.control &= ~256U;
+  CHECK_FALSE(clk_regs.get_flip(gp2_clk_id));
+}
+
+TEST_CASE( "Unit-tests/clock_registers/0060/set_enable"
+         , "Control ENAB bit modified OK by set_enable for given clock"
+         )
+{
+  clock_registers clk_regs;
+// initially start with all bytes of gpio_regs set to 0x00:
+  std::memset(&clk_regs, 0x00, sizeof(clk_regs));
+
+  CHECK(clk_regs.set_enable(pwm_clk_id, true));
+  CHECK(clk_regs.get_enable(pwm_clk_id));
+  CHECK(clk_regs.pwm_clk.control==0x5a000010U);//0x5a000000 is write passwd; 0x10 is ENAB bit
+  CHECK(clk_regs.set_enable(pwm_clk_id, false));
+  CHECK_FALSE(clk_regs.get_enable(pwm_clk_id));
+  CHECK(clk_regs.pwm_clk.control==0x5a000000U);//0x5a000000 is write passwd; ENAB bit off
+  clk_regs.pwm_clk.control=0x12345678U;
+
+  CHECK(clk_regs.set_enable(gp0_clk_id, true, busy_override::yes));
+  CHECK(clk_regs.get_enable(gp0_clk_id));
+  CHECK(clk_regs.gp0_clk.control==0x5a000010U);
+  CHECK(clk_regs.set_enable(gp0_clk_id, false, busy_override::yes));
+  CHECK_FALSE(clk_regs.get_enable(gp0_clk_id));
+  CHECK(clk_regs.gp0_clk.control==0x5a000000U);
+  clk_regs.gp0_clk.control=0x12345678U;
+
+  clk_regs.set_enable(gp1_clk_id, true);
+  CHECK(clk_regs.get_enable(gp1_clk_id));
+  CHECK(clk_regs.gp1_clk.control==0x5a000010U);
+  clk_regs.set_enable(gp1_clk_id, false);
+  CHECK_FALSE(clk_regs.get_enable(gp1_clk_id));
+  CHECK(clk_regs.gp1_clk.control==0x5a000000U);
+  clk_regs.gp1_clk.control=0x12345678U;
+
+  clk_regs.set_enable(gp2_clk_id, true);
+  CHECK(clk_regs.get_enable(gp2_clk_id));
+  CHECK(clk_regs.gp2_clk.control==0x5a000010U);
+  clk_regs.set_enable(gp2_clk_id, false);
+  CHECK_FALSE(clk_regs.get_enable(gp2_clk_id));
+  CHECK(clk_regs.gp2_clk.control==0x5a000000U);
+}
+
+TEST_CASE( "Unit-tests/clock_registers/0070/set_kill"
+         , "Control KILL bit modified OK by set_kill for given clock"
+         )
+{
+  clock_registers clk_regs;
+// initially start with all bytes of gpio_regs set to 0x00:
+  std::memset(&clk_regs, 0x00, sizeof(clk_regs));
+
+  clk_regs.set_kill(pwm_clk_id, true);
+  CHECK(clk_regs.get_kill(pwm_clk_id));
+  CHECK(clk_regs.pwm_clk.control==0x5a000020U);//0x5a000000 is write passwd; 0x20 is KILL bit
+  clk_regs.set_kill(pwm_clk_id, false);
+  CHECK_FALSE(clk_regs.get_kill(pwm_clk_id));
+  CHECK(clk_regs.pwm_clk.control==0x5a000000U);//0x5a000000 is write passwd; KILL bit off
+  clk_regs.pwm_clk.control=0x12345678U;
+
+  clk_regs.set_kill(gp0_clk_id, true);
+  CHECK(clk_regs.get_kill(gp0_clk_id));
+  CHECK(clk_regs.gp0_clk.control==0x5a000020U);
+  clk_regs.set_kill(gp0_clk_id, false);
+  CHECK_FALSE(clk_regs.get_kill(gp0_clk_id));
+  CHECK(clk_regs.gp0_clk.control==0x5a000000U);
+  clk_regs.gp0_clk.control=0x12345678U;
+
+  clk_regs.set_kill(gp1_clk_id, true);
+  CHECK(clk_regs.get_kill(gp1_clk_id));
+  CHECK(clk_regs.gp1_clk.control==0x5a000020U);
+  clk_regs.set_kill(gp1_clk_id, false);
+  CHECK_FALSE(clk_regs.get_kill(gp1_clk_id));
+  CHECK(clk_regs.gp1_clk.control==0x5a000000U);
+  clk_regs.gp1_clk.control=0x12345678U;
+
+  clk_regs.set_kill(gp2_clk_id, true);
+  CHECK(clk_regs.get_kill(gp2_clk_id));
+  CHECK(clk_regs.gp2_clk.control==0x5a000020U);
+  clk_regs.set_kill(gp2_clk_id, false);
+  CHECK_FALSE(clk_regs.get_kill(gp2_clk_id));
+  CHECK(clk_regs.gp2_clk.control==0x5a000000U);
+}
+
+TEST_CASE( "Unit-tests/clock_registers/0080/set_flip"
+         , "Control FLIP bit modified OK by set_flip for given clock"
+         )
+{
+  clock_registers clk_regs;
+// initially start with all bytes of gpio_regs set to 0x00:
+  std::memset(&clk_regs, 0x00, sizeof(clk_regs));
+
+  CHECK(clk_regs.set_flip(pwm_clk_id, true));
+  CHECK(clk_regs.get_flip(pwm_clk_id));
+  CHECK(clk_regs.pwm_clk.control==0x5a000100U);//0x5a000000 is write passwd; 0x100 is FLIP bit
+  CHECK(clk_regs.set_flip(pwm_clk_id, false));
+  CHECK_FALSE(clk_regs.get_flip(pwm_clk_id));
+  CHECK(clk_regs.pwm_clk.control==0x5a000000U);//0x5a000000 is write passwd; ENAB bit off
+  clk_regs.pwm_clk.control=0x12345678U;
+
+  CHECK(clk_regs.set_flip(gp0_clk_id, true, busy_override::yes));
+  CHECK(clk_regs.get_flip(gp0_clk_id));
+  CHECK(clk_regs.gp0_clk.control==0x5a000100U);
+  CHECK(clk_regs.set_flip(gp0_clk_id, false, busy_override::yes));
+  CHECK_FALSE(clk_regs.get_flip(gp0_clk_id));
+  CHECK(clk_regs.gp0_clk.control==0x5a000000U);
+  clk_regs.gp0_clk.control=0x12345678U;
+
+  clk_regs.set_flip(gp1_clk_id, true);
+  CHECK(clk_regs.get_flip(gp1_clk_id));
+  CHECK(clk_regs.gp1_clk.control==0x5a000100U);
+  clk_regs.set_flip(gp1_clk_id, false);
+  CHECK_FALSE(clk_regs.get_flip(gp1_clk_id));
+  CHECK(clk_regs.gp1_clk.control==0x5a000000U);
+  clk_regs.gp1_clk.control=0x12345678U;
+
+  clk_regs.set_flip(gp2_clk_id, true);
+  CHECK(clk_regs.get_flip(gp2_clk_id));
+  CHECK(clk_regs.gp2_clk.control==0x5a000100U);
+  clk_regs.set_flip(gp2_clk_id, false);
+  CHECK_FALSE(clk_regs.get_flip(gp2_clk_id));
+  CHECK(clk_regs.gp2_clk.control==0x5a000000U);
+}
+
+TEST_CASE( "Unit-tests/clock_registers/0090/set_enable for busy clock"
+         , "Check set_flip bit behaviour for busy clock is as expected"
+         )
+{
+  clock_registers clk_regs;
+// initially start with all bytes of gpio_regs set to 0x00:
+  std::memset(&clk_regs, 0x00, sizeof(clk_regs));
+  clk_regs.pwm_clk.control = 128; // BUSY flag is bit 7
+  CHECK_FALSE(clk_regs.set_enable(pwm_clk_id, true));
+  CHECK_FALSE(clk_regs.get_enable(pwm_clk_id));
+  CHECK(clk_regs.pwm_clk.control==128);// BUSY and not forced: no write occured
+  CHECK_FALSE(clk_regs.set_enable(pwm_clk_id, false));
+  CHECK_FALSE(clk_regs.get_enable(pwm_clk_id));
+  CHECK(clk_regs.pwm_clk.control==128);// BUSY and not forced: no write occured
+
+  CHECK(clk_regs.set_enable(pwm_clk_id, true, busy_override::yes));
+  CHECK(clk_regs.get_enable(pwm_clk_id));
+  CHECK(clk_regs.pwm_clk.control==0x5a000090U);//0x5a000000=write password; 0x90=ENAB|BUSY
+  CHECK(clk_regs.set_enable(pwm_clk_id, false, busy_override::yes));
+  CHECK_FALSE(clk_regs.get_enable(pwm_clk_id));
+  CHECK(clk_regs.pwm_clk.control==0x5a000080U);//0x5a000000=write password; 0x80=BUSY, ENAB off
+}
+
+TEST_CASE( "Unit-tests/clock_registers/0100/set_flip for busy clock"
+         , "Check set_flip bit behaviour for busy clock is as expected"
+         )
+{
+  clock_registers clk_regs;
+// initially start with all bytes of gpio_regs set to 0x00:
+  std::memset(&clk_regs, 0x00, sizeof(clk_regs));
+  clk_regs.gp1_clk.control = 128; // BUSY flag is bit 7
+  CHECK_FALSE(clk_regs.set_flip(gp1_clk_id, true));
+  CHECK_FALSE(clk_regs.get_flip(gp1_clk_id));
+  CHECK(clk_regs.gp1_clk.control==128);// BUSY and not forced: no write occured
+  CHECK_FALSE(clk_regs.set_flip(gp1_clk_id, false));
+  CHECK_FALSE(clk_regs.get_flip(gp1_clk_id));
+  CHECK(clk_regs.gp1_clk.control==128);// BUSY and not forced: no write occured
+
+  CHECK(clk_regs.set_flip(gp1_clk_id, true, busy_override::yes));
+  CHECK(clk_regs.get_flip(gp1_clk_id));
+  CHECK(clk_regs.gp1_clk.control==0x5a000180U);//0x5a000000=write password; 0x180=FLIP|BUSY
+  CHECK(clk_regs.set_flip(gp1_clk_id, false, busy_override::yes));
+  CHECK_FALSE(clk_regs.get_flip(gp1_clk_id));
+  CHECK(clk_regs.gp1_clk.control==0x5a000080U);//0x5a000000=write password; 0x80=BUSY, FLIP off
+}

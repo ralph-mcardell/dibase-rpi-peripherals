063083108f0f74e7bb11c91d6af21352782e0fc9
Ralph McArdell <ralph@dibasepi1>
2013-08-23 16:53:55 +0100

    Replaced nested clock_allocator class logic with new simple_allocator class template specialisation

--- src/clock_ctrl.h
+++ src/clock_ctrl.h
@@ -6,10 +6,11 @@
 /// @author Ralph E. McArdell
 
 #ifndef DIBASE_RPI_PERIPHERALS_INTERNAL_CLOCK_CTRL_H
- #define DIBASE_RPI_PERIPHERALS_INTERNAL_CLOCK_CTRL_H
+# define DIBASE_RPI_PERIPHERALS_INTERNAL_CLOCK_CTRL_H
 
 # include "phymem_ptr.h"
 # include "clock_registers.h"
+# include "simple_allocator.h"
 
 namespace dibase { namespace rpi {
   namespace peripherals
@@ -33,66 +34,11 @@ namespace dibase { namespace rpi {
     /// memory mapped area
       struct clock_ctrl
       {
-      /// @brief Simple clock allocation class.
-      ///
-      /// Supports allocate, de-allocate and in_use operations for clocks
-      /// specified using internal clock index values gpclk0, gpclk1, gpclk2
-      /// and pwmclk.
-        class clock_allocator
-        {
-          unsigned allocated;
-
-        public:
-        /// @brief Construct with all clocks available for allocation
-          clock_allocator() : allocated{0U} {}
-
-        /// @brief Return whether a clock is marked as in use or not
-        /// @param clock_idx  Internal clock index value of clock to check
-        ///                   allocation state of: gpclk0, gpclk1, gpclk2 or
-        ///                   pwmclk. Partially range checked.
-        /// @returns true if clock marked as allocated; false if it is not OR
-        ///          clock_idx is out of range.
-          bool is_in_use(unsigned clock_idx)
-          {
-            return (clock_idx<number_of_clocks) && (allocated&(1<<clock_idx));
-          }
-
-        /// @brief Allocate a clock marking it as in use
-        /// @param clock_idx  Internal clock index value of clock to allocate:
-        ///                   gpclk0, gpclk1, gpclk2 or pwmclk. Range checked.
-        /// @returns true if clock was allocated by call; false if it was not
-        ///          as already allocated or clock_idx out of range
-          bool allocate(unsigned clock_idx)
-          {
-            if ( (clock_idx<number_of_clocks) && !is_in_use(clock_idx) )
-              {
-                allocated |= (1<<clock_idx);
-                return true;
-              }
-            return false;
-          }
-
-        /// @brief De-allocate a clock marking it as free for use
-        /// @param clock_idx  Internal clock index of clock to de-allocate:
-        ///                   gpclk0, gpclk1, gpclk2 or pwmclk. Range checked.
-        /// @returns true if clock was de-allocated by call; false if it was not
-        ///          as already free or clock_idx out of range
-          bool deallocate(unsigned clock_idx)
-          {
-            if ( is_in_use(clock_idx) )
-              {
-                allocated &= ~(1<<clock_idx);
-                return true;
-              }
-            return false;
-          }
-        };
-
-        /// @brief Pointer to BCM2708 / BCM2835 clock control registers instance
-        phymem_ptr<volatile clock_registers>   regs;
+      /// @brief Pointer to BCM2708 / BCM2835 clock control registers instance
+        phymem_ptr<volatile clock_registers>  regs;
 
       /// @brief Clock allocator instance
-        clock_allocator                       alloc;
+        simple_allocator<number_of_clocks>    alloc;
 
       /// @brief Singleton instance getter
       /// @returns THE instance of the GPIO clock control object.
---
+++ src/simple_allocator.h
@@ -0,0 +1,77 @@
+// Project: Raspberry Pi BCM2708 / BCM2835 peripherals C++ library
+/// @file simple_allocator.h 
+/// @brief Internal bit-field availability table allocator.
+///
+/// @copyright Copyright (c) Dibase Limited 2013
+/// @author Ralph E. McArdell
+
+#ifndef DIBASE_RPI_PERIPHERALS_INTERNAL_SIMPLE_ALLOCATOR_H
+# define DIBASE_RPI_PERIPHERALS_INTERNAL_SIMPLE_ALLOCATOR_H
+
+# include "phymem_ptr.h"
+# include "pwm_registers.h"
+
+namespace dibase { namespace rpi {
+  namespace peripherals
+  { namespace internal
+    {
+     /// @brief Simple resource allocation class template.
+      ///
+      /// Supports allocate, de-allocate and in_use operations for resources
+      /// specified using resource index values
+      ///
+      /// @param NumRes   Number of resources supported 
+      ///                 (up to bit size of unsigned)
+        template <unsigned NumRes>
+        class simple_allocator
+        {
+          unsigned allocated;
+
+        public:
+        /// @brief Construct with all resources available for allocation
+          simple_allocator() : allocated{0U} {}
+
+        /// @brief Return whether a resource is marked as in use or not
+        /// @param res_idx    Resource index value of resource to check
+        ///                   allocation state of. Partially range checked.
+        /// @returns true if resource marked as allocated; false if it is not
+        ///               OR res_idx is out of range.
+          bool is_in_use(unsigned res_idx)
+          {
+            return (res_idx<NumRes) && (allocated&(1<<res_idx));
+          }
+
+        /// @brief Allocate a resource marking it as in use
+        /// @param res_idx    Resource index value of resource to allocate.
+        ///                   Range checked.
+        /// @returns true if resource was allocated by call; false if it was
+        ///               not as already allocated or res_idx out of range.
+          bool allocate(unsigned res_idx)
+          {
+            if ( (res_idx<NumRes) && !is_in_use(res_idx) )
+              {
+                allocated |= (1<<res_idx);
+                return true;
+              }
+            return false;
+          }
+
+        /// @brief De-allocate a resource marking it as free for use
+        /// @param res_idx    Resource index of resource to de-allocate.
+        ///                   Range checked.
+        /// @returns true if resource was de-allocated by call; false if it
+        ///               was not as already free or res_idx out of range.
+          bool deallocate(unsigned res_idx)
+          {
+            if ( is_in_use(res_idx) )
+              {
+                allocated &= ~(1<<res_idx);
+                return true;
+              }
+            return false;
+          }
+        };
+   } // namespace internal closed
+  } // namespace peripherals closed
+}} // namespaces rpi and dibase closed
+#endif // DIBASE_RPI_PERIPHERALS_INTERNAL_SIMPLE_ALLOCATOR_H

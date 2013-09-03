b9f86c8a06a9e35c451798a97c7d88f8af0424b5
Ralph McArdell <ralph@dibasepi1>
2013-09-03 12:32:33 +0100

    Tidied code - in particular improved exception safety

--- src/examples/pwm-motor.cpp
+++ src/examples/pwm-motor.cpp
@@ -6,8 +6,6 @@
 /// @author Ralph E. McArdell
 //
 
-// ##### Ongoing version. Will be updated when more facilities available #####
-
 #include "pin.h"
 #include "pwm_pin.h"
 
@@ -23,25 +21,24 @@ bool g_running{ true };  ///< Global flag used to communicate quit request
 
 void vary_motor_speed_and_direction()
 {
+  opin direction_pin;
   try
     {
-      pin_id const power_pin_id(gpio_gen1);     // Gertboard GP18 (GPI018)
-      pin_id const direction_pin_id(gpio_gen0); // Gertboard GP17 (GPI017)
-      opin direction_pin{direction_pin_id};
+      direction_pin.open(gpio_gen0); // Gertboard GP17 (GPI017)
       pwm_pin::set_clock( rpi_oscillator
                         , clock_frequency{kilohertz{600}, clock_filter::none}
                         );
-      pwm_pin power_pin{power_pin_id};
+      pwm_pin power_pin{gpio_gen1};  // Gertboard GP18 (GPI018)
       bool direction{false};
       direction_pin.put(direction);
       int power_value{0}; // range [-100,100]; negative values reverse direction
-      int power_change = 1;
-      unsigned const change_wait_ms{200U};
+      int power_change{1};// Start by incrementing speed forwards
+      unsigned const change_wait_ms{200U}; // Delay between changes
       power_pin.start();
     // Ramp speed up by varying power from 0 to 100%, then decrease to zero, 
     // reverse direction, varying power to 100% in reverse, decrease reverse
     // power to zero, ramp forward speed up by varying power...etc. until
-    // user board and quits program run!
+    // user bored and quits program run!
       while (g_running)
         {// When the motor's direction is reversed by raising the direction
          // pin's state we have to invert the PWM high/low ratio
@@ -53,24 +50,26 @@ void vary_motor_speed_and_direction()
           std::cout.flush();
           std::this_thread::sleep_for(std::chrono::milliseconds(change_wait_ms));
           if ( std::abs(power_value)==100 )
-            {  // at range ends switch increase / decrease
+            {  // at range ends switch between increase<->decrease
               power_change *= -1;
             }
-          else if ( power_value==-power_change )
+          else if ( power_value==-power_change ) // ASUMMES power_change 1 or -1
             { // at zero-crossing switch direction pin state
               direction = !direction;
               direction_pin.put(direction);
             }
           power_value += power_change;
         }
-      power_pin.set_ratio(pwm_hundredths(0U));
-      power_pin.stop();
-      direction_pin.put(false);
     }
   catch ( std::exception & e )
     {
       std::cerr << "A problem occurred. Description: " << e.what() << "\n";
     }
+  catch (...)
+    {
+      std::cerr << "A problem occurred. Caught by catch all handler\n";
+    }
+  direction_pin.put(false);
 }
 
 int main()

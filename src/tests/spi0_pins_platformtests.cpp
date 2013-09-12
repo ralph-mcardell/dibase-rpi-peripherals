a8b36e32e286b5202378dc079abdb114c4e08cb9
Ralph McArdell <ralph@dibasepi1>
2013-09-12 14:21:26 +0100

    WIP: Added negative tests for spi0_pins creation.

--- src/tests/spi0_pins_platformtests.cpp
+++ src/tests/spi0_pins_platformtests.cpp
@@ -29,6 +29,7 @@ TEST_CASE( "Platform-tests/spi0_pins/0000/create & destroy with good full pin se
     CHECK(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
     CHECK(spi0_ctrl::instance().allocated);
     CHECK(sp.has_std_mode_support());
+    CHECK_FALSE(sp.has_conversation());
   }
   CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
   CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
@@ -52,6 +53,7 @@ TEST_CASE( "Platform-tests/spi0_pins/0010/create & destroy with good 2 wire pin 
     CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
     CHECK(spi0_ctrl::instance().allocated);
     CHECK_FALSE(sp.has_std_mode_support());
+    CHECK_FALSE(sp.has_conversation());
   }
   CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
   CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
@@ -59,4 +61,151 @@ TEST_CASE( "Platform-tests/spi0_pins/0010/create & destroy with good 2 wire pin 
   CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
   CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
   CHECK_FALSE(spi0_ctrl::instance().allocated);
+}
+
+TEST_CASE( "Platform-tests/spi0_pins/0020/create bad: no expected alt-fn"
+         , "Creating spi0_pins from an SPI0 pin set with a pin that does not "
+           "support the stated special function throws an exception"
+         )
+{
+  spi0_pin_set<0U,7U,11U,10U,9U> full_bad_ce0;
+  spi0_pin_set<8U,0U,11U,10U,9U> full_bad_ce1;
+  spi0_pin_set<8U,7U,0U,10U,9U> full_bad_sclk;
+  spi0_pin_set<8U,7U,11U,0U,9U> full_bad_mosi;
+  spi0_pin_set<8U,7U,11U,10,0U> full_bad_miso;
+  REQUIRE_THROWS_AS((spi0_pins(full_bad_ce0 )), std::invalid_argument);
+  CHECK_FALSE(spi0_ctrl::instance().allocated);
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+  REQUIRE_THROWS_AS((spi0_pins(full_bad_ce1 )), std::invalid_argument);
+  REQUIRE_THROWS_AS((spi0_pins(full_bad_sclk)), std::invalid_argument);
+  REQUIRE_THROWS_AS((spi0_pins(full_bad_mosi)), std::invalid_argument);
+  REQUIRE_THROWS_AS((spi0_pins(full_bad_miso)), std::invalid_argument);
+
+  spi0_pin_set<0U,7U,11U,10U> two_wire_bad_ce0;
+  spi0_pin_set<8U,0U,11U,10U> two_wire_bad_ce1;
+  spi0_pin_set<8U,7U,0U,10U>  two_wire_bad_sclk;
+  spi0_pin_set<8U,7U,11U,0U>  two_wire_bad_mosi;
+  REQUIRE_THROWS_AS((spi0_pins(two_wire_bad_ce0 )), std::invalid_argument);
+  CHECK_FALSE(spi0_ctrl::instance().allocated);
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+  REQUIRE_THROWS_AS((spi0_pins(two_wire_bad_ce1 )), std::invalid_argument);
+  REQUIRE_THROWS_AS((spi0_pins(two_wire_bad_sclk)), std::invalid_argument);
+  REQUIRE_THROWS_AS((spi0_pins(two_wire_bad_mosi)), std::invalid_argument);
+  CHECK_FALSE(spi0_ctrl::instance().allocated);
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+}
+
+TEST_CASE( "Platform-tests/spi0_pins/0030/create bad: SPI0 in use"
+         , "Creating spi0_pins from a good SPI0 pin set when the SPI0 "
+           "peripheral is marked as in use throws an exception"
+         )
+{
+  spi0_ctrl::instance().allocated = true;
+  REQUIRE(spi0_ctrl::instance().allocated);
+
+  REQUIRE_THROWS_AS((spi0_pins(rpi_p1_spi0_full_pin_set)),bad_peripheral_alloc);
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+
+  spi0_ctrl::instance().allocated = false;
+  REQUIRE_FALSE(spi0_ctrl::instance().allocated);
+}
+
+TEST_CASE( "Platform-tests/spi0_pins/0040/create bad: pins in use"
+         , "Creating spi0_pins from a good SPI0 pin set when a pin in the set "
+           "is marked as in use throws an exception"
+         )
+{
+  REQUIRE_FALSE(spi0_ctrl::instance().allocated);
+  REQUIRE_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+  REQUIRE_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+  REQUIRE_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+  REQUIRE_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+  REQUIRE_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+
+
+  gpio_ctrl::instance().alloc.allocate(spi_ce0_n);
+  REQUIRE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+
+  REQUIRE_THROWS_AS((spi0_pins(rpi_p1_spi0_full_pin_set)),bad_peripheral_alloc);
+  CHECK_FALSE(spi0_ctrl::instance().allocated);
+  CHECK(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+
+  gpio_ctrl::instance().alloc.deallocate(spi_ce0_n);
+  REQUIRE_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+
+  gpio_ctrl::instance().alloc.allocate(spi_ce1_n);
+  REQUIRE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+
+  REQUIRE_THROWS_AS((spi0_pins(rpi_p1_spi0_full_pin_set)),bad_peripheral_alloc);
+  CHECK_FALSE(spi0_ctrl::instance().allocated);
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+  CHECK(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+
+  gpio_ctrl::instance().alloc.deallocate(spi_ce1_n);
+  REQUIRE_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+
+  gpio_ctrl::instance().alloc.allocate(spi_sclk);
+  REQUIRE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+
+  REQUIRE_THROWS_AS((spi0_pins(rpi_p1_spi0_full_pin_set)),bad_peripheral_alloc);
+  CHECK_FALSE(spi0_ctrl::instance().allocated);
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+  CHECK(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+
+  gpio_ctrl::instance().alloc.deallocate(spi_sclk);
+  REQUIRE_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+
+  gpio_ctrl::instance().alloc.allocate(spi_mosi);
+  REQUIRE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+
+  REQUIRE_THROWS_AS((spi0_pins(rpi_p1_spi0_full_pin_set)),bad_peripheral_alloc);
+  CHECK_FALSE(spi0_ctrl::instance().allocated);
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+  CHECK(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+
+  gpio_ctrl::instance().alloc.deallocate(spi_mosi);
+  REQUIRE_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+
+  gpio_ctrl::instance().alloc.allocate(spi_miso);
+  REQUIRE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+
+  REQUIRE_THROWS_AS((spi0_pins(rpi_p1_spi0_full_pin_set)),bad_peripheral_alloc);
+  CHECK_FALSE(spi0_ctrl::instance().allocated);
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce0_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_ce1_n));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_sclk));
+  CHECK_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_mosi));
+  CHECK(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
+
+  gpio_ctrl::instance().alloc.deallocate(spi_miso);
+  REQUIRE_FALSE(gpio_ctrl::instance().alloc.is_in_use(spi_miso));
 }

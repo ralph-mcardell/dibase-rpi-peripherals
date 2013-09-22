f2ffd64125b4c5f7c9deae0db39b117b28719dcd
Ralph McArdell <ralph@dibasepi1>
2013-09-22 17:58:56 +0100

    Tweaked interactive tests: added observe-only transfer tests for normal & high priority

--- src/tests/spi0_pins_interactivetests.cpp
+++ src/tests/spi0_pins_interactivetests.cpp
@@ -17,6 +17,8 @@
 #include <future>
 #include <chrono>
 #include <cstdint>
+#include <sched.h>
+#include <string.h>
 
 using namespace dibase::rpi::peripherals;
 using namespace dibase::rpi::peripherals::internal;
@@ -25,23 +27,9 @@ namespace
 {
   auto const short_wait_time(std::chrono::microseconds(5U));
 
-  static void prompt
-  ( std::string prefix
-  , std::string suffix=std::string()
-  , unsigned pin0 = 0U
-  , unsigned pin1 = 0U
-  , unsigned pin2 = 0U
-  , unsigned pin3 = 0U
-  , unsigned pin4 = 0U
-  )
+  static void prompt( std::string text )
   {
-    std::cout << prefix;
-    if (pin0) std::cout << ' ' << pin0;
-    if (pin1) std::cout << ' ' << pin1;
-    if (pin2) std::cout << ' ' << pin2;
-    if (pin3) std::cout << ' ' << pin3;
-    if (pin4) std::cout << ' ' << pin4;
-    if (!suffix.empty()) std::cout << ' ' << suffix;
+    std::cout << text;
     std::string dummy;
     std::getline(std::cin, dummy);
   }
@@ -58,48 +46,66 @@ namespace
     return response.find_first_of("Yy")==0;
   }
 
-  struct fperf // Frequency test performance data type.
+  struct xfer_stats // Transfer performance data.
   {
-    fperf()
+    xfer_stats()
     : rcount{0ULL}
     , wcount{0ULL}
     , rfcount{0ULL}
     , wecount{0ULL}
     , duration_ms{0U}
     , actual_cdiv{0U}
+    , hi_pri{false}
     {}
     
     std::uint64_t rcount;
     std::uint64_t wcount;
-    std::uint64_t rfcount;
-    std::uint64_t wecount;
+
+    unsigned      rfcount; // read full count
+    unsigned      wecount; // write empty count
     
     unsigned duration_ms;
     unsigned actual_cdiv;
+    bool     hi_pri;
   };
 
-  void do_frequency_test(hertz test_frequency, bool const & stop, fperf & perf)
+  bool set_hi_pri()
+  {
+    struct sched_param sched;
+    memset (&sched, 0, sizeof(sched));
+    sched.sched_priority = sched_get_priority_max(SCHED_RR);
+    return sched_setscheduler(0, SCHED_RR, &sched)!=-1;
+  }
+  
+  void do_bkgd_xfer
+  ( hertz test_frequency
+  , bool const & stop, xfer_stats & perf
+  , bool use_hi_pri
+  )
   {
     spi0_pins sp(rpi_p1_spi0_full_pin_set);
     spi0_conversation sc(spi0_slave::chip0, test_frequency);
     sc.open(sp);
     perf.actual_cdiv = spi0_ctrl::instance().regs->get_clock_divider();
-
-  // g++ c++ library has non-standard monotonic_clock rather than steady_clock
     std::uint8_t data{0U};
-    auto const t_start(std::chrono::monotonic_clock::now());
+    std::uint64_t rcount_prev{0ULL};
+    std::uint64_t wcount_prev{0ULL};
+    if (use_hi_pri)
+      {
+        perf.hi_pri = set_hi_pri();
+      }
+    auto const t_start(std::chrono::system_clock::now());
     while ( !stop )
       {
-        if ( sp.write_fifo_is_empty() )   ++perf.wecount;
-        while (sp.read_fifo_has_data())
-          {
-            if (sp.read_fifo_is_full())   ++perf.rfcount;
-            if (sc.read(data))            ++perf.rcount;
-          }
-        if (sc.write(0x5a))               ++perf.wcount;
-        if (test_frequency.count() < 100000U && !sp.write_fifo_has_space())
-          {
-            std::this_thread::yield();
+        rcount_prev = perf.rcount;
+        wcount_prev = perf.wcount;
+        if (sp.read_fifo_is_full())   ++perf.rfcount;
+        if (sc.read(data))            ++perf.rcount;
+        if (sp.write_fifo_is_empty()) ++perf.wecount;
+        if (sc.write(0x5a))           ++perf.wcount;
+        if (perf.rcount==rcount_prev && perf.wcount==wcount_prev)
+          { // nothing read or written - let something else run...
+            std::this_thread::sleep_for(std::chrono::microseconds(2));
           }
       }
     while (sp.read_fifo_has_data() || !sp.write_fifo_is_empty())
@@ -112,7 +118,7 @@ namespace
                         <std::chrono::milliseconds>(t_elapsed).count();
   }
 
-  bool test_clock_frequency(hertz f)
+  std::string hertz_to_string(hertz f)
   {
     std::string f_units("Hz");
     double f_display(f.count());
@@ -127,24 +133,55 @@ namespace
         f_display = f_kilohertz(f).count();
       }
     std::ostringstream oss;
-    oss << "Expect " << f_display << f_units 
-        << " clock waveform. Press <enter> when ready...";
-    prompt(oss.str());
+    oss << f_display << f_units;
+    return oss.str();
+  }
+  
+  bool test_transfer(hertz f, bool use_hi_pri=false)
+  {
+    std::cout << "Transfer at " << hertz_to_string(f) 
+              << " for approximately 1 second...\n";
     bool stop{false};
-    fperf perf;
-    std::thread spi_run(do_frequency_test, f, std::cref(stop), std::ref(perf));
-    oss.str("");
-    oss << "Has the SCLK output a " << f_display << f_units << " frequency";
-    bool result(yn_query(oss.str().c_str()));
+    xfer_stats perf;
+    std::thread spi_run(do_bkgd_xfer, f, std::cref(stop), std::ref(perf), use_hi_pri);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     stop = true;
     spi_run.join();
+    --perf.wecount; // discount count for initial empty state
     std::cout << "Wrote " << perf.wcount 
               << " bytes (FIFO empty " << perf.wecount 
               << " times), read " << perf.rcount 
               << " bytes (FIFO full " << perf.rfcount 
               << " times) in " << perf.duration_ms 
               << "ms using a CDIV of " << perf.actual_cdiv 
-              << "\n\n";
+              << (perf.hi_pri?"  HIGH":"  NORMAL") << " priority"
+              << '\n';
+    double scale_to_per_sec(1000.0/perf.duration_ms);
+    std::cout << "      " << std::uint64_t(perf.wcount*scale_to_per_sec+0.5)
+              << " Bps               " 
+              << unsigned(perf.wecount*scale_to_per_sec+0.5)
+              << "/sec          " 
+              <<  std::uint64_t(perf.rcount*scale_to_per_sec+0.5)
+              << " Bps              " 
+              << unsigned(perf.rfcount*scale_to_per_sec+0.5)
+              << "/sec\n\n";
+    return true;
+  }
+
+  bool test_clock_frequency(hertz f)
+  {
+    std::ostringstream oss;
+    oss << "Expect " <<  hertz_to_string(f)
+        << " clock waveform. Press <enter> when ready...";
+    prompt(oss.str());
+    bool stop{false};
+    xfer_stats perf;
+    std::thread spi_run(do_bkgd_xfer, f, std::cref(stop), std::ref(perf), false);
+    oss.str("");
+    oss << "Has the SCLK output a " <<  hertz_to_string(f) << " frequency";
+    bool result(yn_query(oss.str().c_str()));
+    stop = true;
+    spi_run.join();
     return result;
   }
 
@@ -226,7 +263,41 @@ TEST_CASE( "Interactive_tests/spi0_pins/0000/read write standard SPI"
     }
 }
 
-TEST_CASE( "Interactive_tests/spi0_pins/0010/clock frequency"
+TEST_CASE( "Interactive_tests/spi0_pins/0020/fifo full,empty: normal priority"
+         , "Observe the ability of a program to continuously keep data in the "
+           "TX FIFO and prevent the RX FIFO filling"
+         )
+{
+  welcome();
+  std::cout << "\nSPI0 continuous transfer tests @ NORMAL priority\n\n";
+ 
+  CHECK(test_transfer(kilohertz(5)));
+  CHECK(test_transfer(kilohertz(50)));
+  CHECK(test_transfer(kilohertz(100)));
+  CHECK(test_transfer(kilohertz(200)));
+  CHECK(test_transfer(kilohertz(500)));
+  CHECK(test_transfer(megahertz(1)));
+  CHECK(test_transfer(megahertz(2)));
+}
+
+TEST_CASE( "Interactive_tests/spi0_pins/0030/fifo full,empty: high priority"
+         , "Observe the ability of a program to continuously keep data in the "
+           "TX FIFO and prevent the RX FIFO filling"
+         )
+{
+  welcome();
+  std::cout << "\nSPI0 continuous transfer tests @ HIGH priority\n\n";
+
+  CHECK(test_transfer(kilohertz(5),true));
+  CHECK(test_transfer(kilohertz(50),true));
+  CHECK(test_transfer(kilohertz(100),true));
+  CHECK(test_transfer(kilohertz(200),true));
+  CHECK(test_transfer(kilohertz(500),true));
+  CHECK(test_transfer(megahertz(1),true));
+  CHECK(test_transfer(megahertz(2),true));
+}
+
+TEST_CASE( "Interactive_tests/spi0_pins/0040/clock frequency"
          , "Check the CLK pin frequency is as expected"
          )
 {

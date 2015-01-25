edf155da236060f42a4a00f4e96bb75d67fb6703
Ralph McArdell <ralph@dibasepi1>
2015-01-25 17:27:36 +0000

    Deleted src/examples/led-string-display/config-file-tests.{cpp|h}.

--- src/examples/led-string-display/config-file-tests.cpp
+++
@@ -1,8 +0,0 @@
-// Project: Raspberry Pi BCM2708 / BCM2835 peripherals C++ library
-/// @file examples/led-string-display/config-file-tests.cpp
-/// @brief Quick & dirty tests for led-string-display.cpp config. file support.
-//
-/// @copyright Copyright (c) Dibase Limited 2015
-/// @author Ralph E. McArdell
-
-#error config-file-tests.cpp is no longer required.
